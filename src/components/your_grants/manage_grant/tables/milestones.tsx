9f1dfb65833ba225264e6069c34a2166463aec51
Adhiraj Singh <adhirajsingh1001@gmail.com>
2022-02-15 16:25:09 +0530

    chore: abstract away core milestone table UI

--- src/components/your_grants/manage_grant/tables/milestones.tsx
+++ src/components/your_grants/manage_grant/tables/milestones.tsx
@@ -7,46 +7,19 @@ import {
 } from '@chakra-ui/icons';
 import moment from 'moment';
 import { ApplicationMilestone } from 'src/graphql/queries';
-import { getAssetInfo } from 'src/utils/tokenUtils';
+import AbstractMilestonesTable, { AbstractMilestonesTableProps } from 'src/components/ui/tables/AbstractMilestonesTable';
 import Modal from '../../../ui/modal';
 import MilestoneDoneModalContent from '../modals/modalContentMilestoneDone';
 import MilestoneViewModalContent from '../modals/modalContentMilestoneView';
 import MilestoneDoneConfirmationModalContent from '../modals/modalContentMilestoneDoneConfirmation';
 
-const TABLE_HEADERS = [
-  {
-    title: 'Milestone',
-    flex: 0.504,
-    justifyContent: 'flex-start',
-  },
-  {
-    title: 'Reward / Expected Reward',
-    flex: 0.358,
-    justifyContent: 'flex-start',
-  },
-  {
-    title: 'Status',
-    flex: 0.138,
-
-    justifyContent: 'center',
-  },
-];
-
-export type MilestoneTableProps = {
-  milestones: ApplicationMilestone[]
-  rewardAssetId: string
-  loading: boolean
-  error: Error
-};
-
-function Milestones({ milestones, rewardAssetId }: MilestoneTableProps) {
+function Milestones(props: Omit<AbstractMilestonesTableProps, 'renderStatus'>) {
   const [isMilestoneDoneModalOpen, setIsMilestoneDoneModalOpen] = React.useState(false);
   const [isMilestoneViewModalOpen, setIsMilestoneViewModalOpen] = React.useState(false);
   const [
     isMilestoneDoneConfirmationModalOpen,
     setIsMilestoneDoneConfirmationModalOpen,
   ] = React.useState(false);
-  const { icon: rewardIcon, label: rewardSymbol } = getAssetInfo(rewardAssetId);
 
   const renderStatus = (status: ApplicationMilestone['state'], updatedAtS: number) => {
     if (status === 'submitted') {
@@ -160,109 +133,11 @@ function Milestones({ milestones, rewardAssetId }: MilestoneTableProps) {
   };
 
   return (
-    <Flex
-      w="100%"
-      my={4}
-      align="center"
-      direction="column"
-      flex={1}
-    >
-      <Flex
-        direction="row"
-        w="100%"
-        justify="strech"
-        align="center"
-        mt="32px"
-        mb="9px"
-      >
-        {TABLE_HEADERS.map((header) => (
-          <Text
-            justifyContent={header.justifyContent}
-            flex={header.flex ? header.flex : 1}
-            variant="tableHeader"
-            display="flex"
-            minW="180px"
-          >
-            {header.title}
-          </Text>
-        ))}
-      </Flex>
-      <Flex
-        direction="column"
-        w="100%"
-        border="1px solid #D0D3D3"
-        borderRadius={4}
-        align="stretch"
-      >
-        {milestones.map((item, index) => (
-          <Flex
-            direction="row"
-            w="100%"
-            justify="stretch"
-            align="center"
-            bg={index % 2 === 0 ? '#F7F9F9' : 'white'}
-            px={0}
-            py={4}
-          >
-            <Flex
-              flex={TABLE_HEADERS[0].flex}
-              direction="column"
-              w="100%"
-              pl="19px"
-            >
-              <Text variant="applicationText" fontWeight="700" color="#122224">
-                {item.title}
-              </Text>
-              <Text
-                fontSize="14px"
-                lineHeight="24px"
-                letterSpacing={0.5}
-                fontStyle="normal"
-                fontWeight="400"
-                color="#717A7C"
-                noOfLines={1}
-                textOverflow="ellipsis"
-              >
-                Milestone
-                {' '}
-                {index + 1}
-              </Text>
-            </Flex>
-            <Flex
-              ml={8}
-              direction="row"
-              justify="start"
-              align="center"
-              flex={TABLE_HEADERS[1].flex}
-            >
-              <Image display="inline-block" src={rewardIcon} mr={2} boxSize="27px" />
-              <Text
-                textAlign="center"
-                fontSize="14px"
-                letterSpacing={0.5}
-                fontWeight="700"
-                color="#122224"
-              >
-                {item.amountPaid}
-                {' '}
-                /
-                {' '}
-                {item.amount}
-                {' '}
-                {rewardSymbol}
-              </Text>
-            </Flex>
-            <Flex
-              flex={TABLE_HEADERS[2].flex}
-              justify="end"
-              mr={5}
-              minW="180px"
-            >
-              {renderStatus(item.state, item.updatedAtS || 0)}
-            </Flex>
-          </Flex>
-        ))}
-      </Flex>
+    <>
+      <AbstractMilestonesTable
+        {...props}
+        renderStatus={(milestone) => renderStatus(milestone.state, milestone.updatedAtS || 0)}
+      />
       <Modal
         isOpen={isMilestoneDoneModalOpen}
         onClose={() => setIsMilestoneDoneModalOpen(false)}
@@ -297,7 +172,7 @@ function Milestones({ milestones, rewardAssetId }: MilestoneTableProps) {
           onClose={() => setIsMilestoneDoneConfirmationModalOpen(false)}
         />
       </Modal>
-    </Flex>
+    </>
   );
 }
 
