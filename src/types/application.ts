5438aba71057a805c6a3bfc9365f3d7181dde684
Adhiraj Singh <adhirajsingh1001@gmail.com>
2022-02-21 16:57:26 +0530

    feat: type up grant application query + add getStringField util

--- src/types/application.ts
+++ src/types/application.ts
@@ -2,12 +2,12 @@ export interface GrantApplicationProps {
   applicantName: string;
   applicantEmail:string;
   teamMembers: number;
-  membersDescription: [{ description: string }];
+  membersDescription: { description: string }[];
   projectName: string;
-  projectLinks: [{ link: string }];
+  projectLinks: { link: string }[];
   projectDetails: string;
   projectGoal: string;
-  projectMilestones: [{ milestone: string, milestoneReward: string }];
+  projectMilestones: { milestone: string, milestoneReward: string }[];
   fundingAsk: string;
   fundingBreakdown: string;
 
