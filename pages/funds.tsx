bf1f5525df8193d4895f97748e84f439c38a2eb6
Richik Chanda <richikchanda1999@gmail.com>
2022-02-22 17:13:01 +0530

    No funds empty state solved

--- pages/funds.tsx
+++ pages/funds.tsx
@@ -1,5 +1,7 @@
 import { Flex, Text } from '@chakra-ui/react';
 import React, { ReactElement, useContext } from 'react';
+import Empty from 'src/components/ui/empty';
+import { useRouter } from 'next/router';
 import { useAllGrantsForDAO } from '../src/graphql/queries';
 import NavbarLayout from '../src/layout/navbarLayout';
 import FundForAGrant from '../src/components/funds';
@@ -8,16 +10,31 @@ import { ApiClientsContext } from './_app';
 function AddFunds() {
   const workspaceId = useContext(ApiClientsContext)?.workspaceId;
   const { data } = useAllGrantsForDAO(workspaceId!);
-
+  const router = useRouter();
   return (
     <Flex direction="row" justify="center">
       <Flex w="80%" direction="column" align="start" mt={6}>
         <Text variant="heading">Funds</Text>
         {
-          data.map(
+          data.length > 0 && data.map(
             (grant) => <FundForAGrant grant={grant} />,
           )
         }
+        {data.length === 0 && (
+          <Flex direction="column" align="center" w="100%" h="100%" mt={14}>
+            <Empty
+              src={`/illustrations/empty_states/${router.query.done ? 'first_grant.svg' : 'no_grants.svg'}`}
+              imgHeight="174px"
+              imgWidth="146px"
+              title={router.query.done
+                ? 'Your grant is being published..'
+                : 'It’s quite silent here!'}
+              subtitle={router.query.done
+                ? 'You may visit this page after a while to see the published grant. Once published, the grant will be live and will be open for anyone to apply.'
+                : 'Get started by creating your grant and post it in less than 2 minutes.'}
+            />
+          </Flex>
+        )}
       </Flex>
     </Flex>
   );
