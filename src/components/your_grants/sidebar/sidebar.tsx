a38e051e5af84fa76eeee2cff581164a4f30ec3b
Sahil Vasava <sahilvasava@gmail.com>
2022-03-15 16:04:41 +0530

    fix: change submit pubkey cta order and add learn more link

--- src/components/your_grants/sidebar/sidebar.tsx
+++ src/components/your_grants/sidebar/sidebar.tsx
@@ -1,5 +1,5 @@
 import {
-  Flex, Text, Image, Box, Button,
+  Flex, Text, Image, Box, Button, Link,
 } from '@chakra-ui/react';
 import { useRouter } from 'next/router';
 import React, { useEffect, useState } from 'react';
@@ -16,36 +16,42 @@ function Sidebar({ showCreateGrantItem }: Props) {
   const [listItems, setListItems] = useState<any[]>([]);
 
   useEffect(() => {
-    let items = [{
-      icon: '/ui_icons/first_grant.svg',
-      title: 'Get access to encrypted applicant data',
-      subtitle: 'Provide access to your public encryption key ',
-      onSubmit: () => {
-        setHiddenModalOpen(true);
+    let items = [
+      {
+        icon: '/ui_icons/first_grant.svg',
+        title: 'Create your first grant',
+        subtitle: 'Add grant details, set a reward and attract applicants. ',
+        onSubmit: () => {
+          router.push({
+            pathname: '/your_grants/create_grant/',
+          });
+        },
       },
-    },
-    {
-      icon: '/ui_icons/first_grant.svg',
-      title: 'Create your first grant',
-      subtitle: 'Add grant details, set a reward and attract applicants. ',
-      onSubmit: () => {
-        router.push({
-          pathname: '/your_grants/create_grant/',
-        });
+      {
+        icon: '/ui_icons/first_grant.svg',
+        title: 'Get access to encrypted applicant data',
+        subtitle: 'Provide access to your public encryption key ',
+        learnMoreText: 'Why is this required?',
+        learnMoreLink:
+          'https://www.notion.so/questbook/Why-is-public-key-required-e3fa53f34a5240d185d3d34744bb33f4',
+        onSubmit: () => {
+          setHiddenModalOpen(true);
+        },
       },
-    }, {
-      icon: '/ui_icons/invite_team.svg',
-      title: 'Invite Team',
-      subtitle: 'Invite core DAO members to set up grants, and add funds to it.',
-      onSubmit: () => {
-        router.push({
-          pathname: '/settings_and_members/',
-          query: {
-            tab: 'members',
-          },
-        });
+      {
+        icon: '/ui_icons/invite_team.svg',
+        title: 'Invite Team',
+        subtitle:
+          'Invite core DAO members to set up grants, and add funds to it.',
+        onSubmit: () => {
+          router.push({
+            pathname: '/settings_and_members/',
+            query: {
+              tab: 'members',
+            },
+          });
+        },
       },
-    },
     ];
     if (!showCreateGrantItem) {
       items = items.filter((item) => item.title !== 'Create your first grant');
@@ -68,17 +74,18 @@ function Sidebar({ showCreateGrantItem }: Props) {
         },
       ]}
     >
-      <Flex
-        flex={1}
-        w="100%"
-        direction="column"
-        py={7}
-        px={10}
-      >
+      <Flex flex={1} w="100%" direction="column" py={7} px={10}>
         <Text variant="heading">
           Here is what you can do
           {' '}
-          <Text as="span" display="inline-block" color="#EA5050" variant="heading" fontWeight="600" letterSpacing={-1}>
+          <Text
+            as="span"
+            display="inline-block"
+            color="#EA5050"
+            variant="heading"
+            fontWeight="600"
+            letterSpacing={-1}
+          >
             next?
           </Text>
         </Text>
@@ -106,11 +113,30 @@ function Sidebar({ showCreateGrantItem }: Props) {
               >
                 {item.subtitle}
               </Text>
-              <Button mt={5} variant="primaryCta" height="32px" onClick={item.onSubmit}>Start</Button>
+              {item.learnMoreText && (
+                <Link href={item.learnMoreLink} mt={2} isExternal>
+                  <Text
+                    color="#122224"
+                    fontWeight="normal"
+                    fontSize="14px"
+                    lineHeight="20px"
+                    decoration="underline"
+                  >
+                    {item.learnMoreText}
+                  </Text>
+                </Link>
+              )}
+              <Button
+                mt={5}
+                variant="primaryCta"
+                height="32px"
+                onClick={item.onSubmit}
+              >
+                Start
+              </Button>
             </Flex>
           </Flex>
         ))}
-
       </Flex>
       <RenderModal />
     </SidebarComponent>
