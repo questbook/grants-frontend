c7278ea271a8191bb29b157ba4c80f2b80714ff2
AlwaysHungrie <dhairyashah98@gmail.com>
2022-03-17 10:10:01 +0530

    links in rich text editor

--- src/components/ui/forms/richTextEditor/textViewer.tsx
+++ src/components/ui/forms/richTextEditor/textViewer.tsx
@@ -1,6 +1,8 @@
 /* eslint-disable jsx-a11y/click-events-have-key-events */
-import React, { useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 import {
+  ContentState,
+  convertFromRaw,
   EditorState,
 } from 'draft-js';
 import Editor, { composeDecorators } from '@draft-js-plugins/editor';
@@ -8,18 +10,64 @@ import createImagePlugin from '@draft-js-plugins/image';
 import createResizeablePlugin from '@draft-js-plugins/resizeable';
 import 'draft-js/dist/Draft.css';
 import '@draft-js-plugins/image/lib/plugin.css';
+import { Link } from '@chakra-ui/react';
+import createLinkifyPlugin from '@draft-js-plugins/linkify';
 
+const linkifyPlugin = createLinkifyPlugin({
+  component(props) {
+    // eslint-disable-next-line no-alert, jsx-a11y/anchor-has-content
+    // return <a {...props} onClick={() => alert('Clicked on Link!')} />;
+    return (
+      <Link {...props} onClick={() => window.open(props.href, '_blank')} isExternal />
+    );
+  },
+});
 const resizeablePlugin = createResizeablePlugin();
 const decorator = composeDecorators(resizeablePlugin.decorator);
 const imagePlugin = createImagePlugin({ decorator });
-const plugins = [resizeablePlugin, imagePlugin];
+const plugins = [resizeablePlugin, imagePlugin, linkifyPlugin];
 
 function TextViewer({
-  value: editorState,
+  // value: editorState,
+  // onChange: setEditorState,
+  grantDetails,
 }: {
-  value: EditorState;
+  // value: EditorState;
+  // onChange: (editorState: EditorState) => void;
+  grantDetails: string;
 }) {
   const ref = useRef(null);
+  const [editorState, setEditorState] = React.useState(() => {
+    try {
+      const o = JSON.parse(grantDetails);
+      return EditorState.createWithContent(convertFromRaw(o));
+    } catch (e) {
+      if (grantDetails) {
+        return EditorState.createWithContent(ContentState.createFromText(grantDetails));
+      }
+      return EditorState.createEmpty();
+    }
+  });
+
+  useEffect(() => {
+    try {
+      const o = JSON.parse(grantDetails);
+      const newState = EditorState.createWithContent(convertFromRaw(o));
+      EditorState.push(newState, ContentState.createFromText(grantDetails), 'change-block-data');
+    } catch (e) {
+      if (grantDetails) {
+        const newState = EditorState.createWithContent(ContentState.createFromText(grantDetails));
+        EditorState.push(newState, ContentState.createFromText(grantDetails), 'change-block-data');
+      } else {
+        const newState = EditorState.createEmpty();
+        EditorState.push(newState, ContentState.createFromText(grantDetails), 'change-block-data');
+      }
+    }
+  }, [grantDetails]);
+
+  const onChange = (state: EditorState) => {
+    setEditorState(state);
+  };
 
   function getBlockStyle(block: any) {
     switch (block.getType()) {
@@ -44,11 +92,11 @@ function TextViewer({
         blockStyleFn={getBlockStyle}
         ref={ref}
         editorState={editorState}
-        editorKey="foobar"
+        editorKey="foo"
         spellCheck={false}
-        readOnly
         plugins={plugins}
-        onChange={() => {}}
+        onChange={onChange}
+        readOnly
       />
     </div>
   );
