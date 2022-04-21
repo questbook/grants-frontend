993b07f29473d98dc43ab7eabc8be51cd8690d14
AlwaysHungrie <dhairyashah98@gmail.com>
2022-04-21 09:01:04 +0530

    all supported networks except polygon

--- src/constants/chains.ts
+++ src/constants/chains.ts
@@ -1,8 +1,8 @@
 export enum SupportedChainId {
   RINKEBY = 4,
-  // HARMONY_TESTNET_S0 = 1666700000,
-  // POLYGON_TESTNET = 80001,
-  // POLYGON_MAINNET = 137,
+  HARMONY_TESTNET_S0 = 1666700000,
+  POLYGON_TESTNET = 80001,
+  POLYGON_MAINNET = 137,
 }
 
 export const ALL_SUPPORTED_CHAIN_IDS: SupportedChainId[] = Object.values(SupportedChainId).filter(
--- src/utils/validationUtils.tsx
+++ src/utils/validationUtils.tsx
@@ -15,19 +15,16 @@ const isValidEmail = (email: string) => {
 const getSupportedChainIdFromSupportedNetwork = (chainId: SupportedNetwork) => {
   if (chainId === SupportedNetwork.Chain_4) {
     return SupportedChainId.RINKEBY;
+  } if (chainId === SupportedNetwork.Chain_80001) {
+    return SupportedChainId.POLYGON_TESTNET;
+  } if (chainId === SupportedNetwork.Chain_137) {
+    return SupportedChainId.POLYGON_MAINNET;
   }
-  return SupportedChainId.RINKEBY;
-  // commenting
-  // } if (chainId === SupportedNetwork.Chain_80001) {
-  //   return SupportedChainId.POLYGON_TESTNET;
-  // } if (chainId === SupportedNetwork.Chain_137) {
-  //   return SupportedChainId.POLYGON_MAINNET;
+  // @TODO: needs type for harmony
+  // if (chainId === SupportedNetwork.Chain_80001) {
+  //   return SupportedChainId.HARMONY_TESTNET_S0;
   // }
-  // // @TODO: needs type for harmony
-  // // if (chainId === SupportedNetwork.Chain_80001) {
-  // //   return SupportedChainId.HARMONY_TESTNET_S0;
-  // // }
-  // return SupportedChainId.HARMONY_TESTNET_S0;
+  return SupportedChainId.HARMONY_TESTNET_S0;
   // cannot return undefined ?
   // return undefined;
 };
@@ -42,21 +39,18 @@ const getSupportedChainIdFromWorkspace = (workspace?: MinimalWorkspace) => {
 
 // eslint-disable-next-line arrow-body-style
 const getSupportedValidatorNetworkFromChainId = (chainId: SupportedChainId) => {
-  return SupportedValidatorNetwork._4;
-
-  // commenting
-  // if (chainId === SupportedChainId.RINKEBY) {
-  //   return SupportedValidatorNetwork._4;
-  // } if (chainId === SupportedChainId.POLYGON_TESTNET) {
-  //   return SupportedValidatorNetwork._80001;
-  // } if (chainId === SupportedChainId.POLYGON_MAINNET) {
-  //   return SupportedValidatorNetwork._137;
-  // }
+  if (chainId === SupportedChainId.RINKEBY) {
+    return SupportedValidatorNetwork._4;
+  } if (chainId === SupportedChainId.POLYGON_TESTNET) {
+    return SupportedValidatorNetwork._80001;
+  } if (chainId === SupportedChainId.POLYGON_MAINNET) {
+    return SupportedValidatorNetwork._137;
+  }
   // @TODO: needs type for harmony
   // if (chainId === SupportedNetwork.Chain_80001) {
   //   return SupportedChainId.HARMONY_TESTNET_S0;
   // }
-  // return SupportedValidatorNetwork._1666700000;
+  return SupportedValidatorNetwork._1666700000;
 };
 
 export {
