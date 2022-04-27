0096af75c5c6fc2bbba62d01a5ad5c87e5de395e
Angel <angel333gomez@gmail.com>
2022-04-26 22:19:40 -0400

    added new smart contract

--- src/hooks/usePayReviewers.ts
+++ src/hooks/usePayReviewers.ts
@@ -1,39 +1,41 @@
 import React, { useContext, useEffect } from 'react';
-import { ToastId, useToast } from '@chakra-ui/react';
+// import { ToastId, useToast } from '@chakra-ui/react';
 import { ApiClientsContext } from 'pages/_app';
-import { useAccount, useNetwork } from 'wagmi';
+// import { useAccount, useNetwork } from 'wagmi';
 import { getSupportedChainIdFromWorkspace } from 'src/utils/validationUtils';
 import { BigNumber } from 'ethers';
-import getErrorMessage from 'src/utils/errorUtils';
+// import getErrorMessage from 'src/utils/errorUtils';
 import { CHAIN_INFO } from 'src/constants/chainInfo';
-import ErrorToast from '../components/ui/toasts/errorToast';
+// import ErrorToast from '../components/ui/toasts/errorToast';
 import useChainId from './utils/useChainId';
-import useERC20Contract from './contracts/useERC20Contract';
+import useApplicationRegistryContract from './contracts/useApplicationRegistryContract';
+
 
 export default function usePayReviewers(
   totalAmount?: BigNumber,
-  reviewerAddress?: string,
-  reviewCurrencyAddress?: string,
+  // reviewerAddress?: string,
+  // reviewCurrencyAddress?: string,
 ) {
   const [error, setError] = React.useState<string>();
-  const [loading, setLoading] = React.useState(false);
+  const [loading] = React.useState(false);
   const [incorrectNetwork, setIncorrectNetwork] = React.useState(false);
   const [transactionData, setTransactionData] = React.useState<any>();
-  const [{ data: accountData }] = useAccount();
-  const [{ data: networkData }, switchNetwork] = useNetwork();
+  // const [{ data: accountData }] = useAccount();
+  // const [{ data: networkData }, switchNetwork] = useNetwork();
 
   const apiClients = useContext(ApiClientsContext)!;
   const { workspace } = apiClients;
-  const rewardContract = useERC20Contract(reviewCurrencyAddress);
-  const toastRef = React.useRef<ToastId>();
-  const toast = useToast();
-  const currentChainId = useChainId();
   const chainId = getSupportedChainIdFromWorkspace(workspace);
+  const applicationReviewerContract = useApplicationRegistryContract(chainId);
+  // const toastRef = React.useRef<ToastId>();
+  // const toast = useToast();
+  const currentChainId = useChainId();
 
   useEffect(() => {
     if (totalAmount) {
       setError(undefined);
       setIncorrectNetwork(false);
+      error
     } else if (transactionData) {
       setTransactionData(undefined);
       setIncorrectNetwork(false);
@@ -45,109 +47,7 @@ export default function usePayReviewers(
       setIncorrectNetwork(false);
     }
   // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [rewardContract]);
-
-  useEffect(() => {
-    if (incorrectNetwork) return;
-    if (error) return;
-    if (loading) return;
-    // console.log('calling createGrant');
-
-    async function validate() {
-      setLoading(true);
-      // console.log('calling validate');
-      try {
-        const transferTxn = await rewardContract.transfer(
-          reviewerAddress,
-          totalAmount,
-        );
-        const depositTransactionData = await transferTxn.wait();
-
-        setTransactionData(depositTransactionData);
-        setLoading(false);
-      } catch (e: any) {
-        const message = getErrorMessage(e);
-        setError(message);
-        setLoading(false);
-        toastRef.current = toast({
-          position: 'top',
-          render: () => ErrorToast({
-            content: message,
-            close: () => {
-              if (toastRef.current) {
-                toast.close(toastRef.current);
-              }
-            },
-          }),
-        });
-      }
-    }
-    try {
-      if (!totalAmount) return;
-      if (!reviewerAddress) return;
-      if (transactionData) return;
-      if (!accountData || !accountData.address) {
-        throw new Error('not connected to wallet');
-      }
-      if (!workspace) {
-        throw new Error('not connected to workspace');
-      }
-      if (!currentChainId) {
-        if (switchNetwork && chainId) { switchNetwork(chainId); }
-        setIncorrectNetwork(true);
-        setLoading(false);
-        return;
-      }
-      if (chainId !== currentChainId) {
-        if (switchNetwork && chainId) { switchNetwork(chainId); }
-        setIncorrectNetwork(true);
-        setLoading(false);
-        return;
-      }
-      if (totalAmount.isZero()) throw new Error('Amount entered should be more than 0!');
-      if (totalAmount.isNegative()) throw new Error('Amount entered cannot be negative!');
-      if (
-        !rewardContract
-        || rewardContract.address
-          === '0x0000000000000000000000000000000000000000'
-        || !rewardContract.signer
-        || !rewardContract.provider
-      ) {
-        return;
-      }
-      validate();
-    } catch (e: any) {
-      const message = getErrorMessage(e);
-      setError(message);
-      setLoading(false);
-      toastRef.current = toast({
-        position: 'top',
-        render: () => ErrorToast({
-          content: message,
-          close: () => {
-            if (toastRef.current) {
-              toast.close(toastRef.current);
-            }
-          },
-        }),
-      });
-    }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [
-    error,
-    loading,
-    toast,
-    transactionData,
-    rewardContract,
-    workspace,
-    accountData,
-    networkData,
-    currentChainId,
-    reviewerAddress,
-    totalAmount,
-    chainId,
-    incorrectNetwork,
-  ]);
+  }, [applicationReviewerContract]);
 
   return [
     transactionData,
