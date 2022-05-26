79bebe0cbcedd23ce7157be6ddbf2a1e718f89f6
Richik Chanda <richikchanda1999@gmail.com>
2022-05-26 15:32:00 +0530

    Removed console statement

--- src/utils/validationUtils.tsx
+++ src/utils/validationUtils.tsx
@@ -9,7 +9,6 @@ const isValidAddress = (address: string) => ethers.utils.isAddress(address)
 const isValidEmail = (email: string) => {
 	// eslint-disable-next-line no-useless-escape
 	const regexp = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-	console.log('Email: ', email, ', isValid: ', regexp.test(email))
 	return regexp.test(email)
 }
 
