6fa3a099096a333e0ab722e46d5a5bee6923da3f
Angel <angel333gomez@gmail.com>
2022-05-31 09:14:32 -0400

    fixed payouts

--- src/components/manage_dao/payouts.tsx
+++ src/components/manage_dao/payouts.tsx
@@ -18,6 +18,7 @@ import {
 } from '@chakra-ui/react'
 import { utils } from 'ethers'
 import router from 'next/router'
+import { CHAIN_INFO } from 'src/constants/chainInfo'
 import { SupportedChainId } from 'src/constants/chains'
 import { useGetDaoGrantsQuery, useGetFundSentforReviewsQuery } from 'src/generated/graphql'
 // TOOLS AND UTILS
@@ -36,10 +37,15 @@ import PayoutModalContent from './payoutModalContent'
 
 function Payouts() {
 	const { subgraphClients, workspace } = useContext(ApiClientsContext)!
+	const [workspaceChainId, setWorkspaceChainId] = React.useState<number>()
 	const [applications, setApplications] = React.useState<any>([])
 	const [outstandingReviews, setOutstandingReviews] = React.useState<any>([])
 	const [reviewers, setReviewers] = React.useState<any>([])
 	const [reviewPayoutsDone, setReviewPayoutsDone] = React.useState<any>([])
+
+	React.useEffect(() => {
+		setWorkspaceChainId(getSupportedChainIdFromWorkspace(workspace))
+	}, [workspace])
 
 	const { data: grantsData } = useGetDaoGrantsQuery({
 		client:
@@ -613,7 +619,15 @@ There is no payout history to show
 
 													<Flex direction="row">
 														<Link
-															href={`http://www.polygonscan.com/tx/${data.id.substr(0, data.id.indexOf('.'))}`}
+															href={
+																workspaceChainId ?
+																	`${CHAIN_INFO[workspaceChainId]
+																		.explorer.transactionHash}${data.id.substr(
+																		0,
+																		data.id.indexOf('.'),
+																	)}`
+																	: ''
+															}
 															isExternal
 														>
                             View
