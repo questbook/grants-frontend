200d30555c161fa13cbac020cd2f208fa5702768
Adhiraj Singh <adhirajsingh1001@gmail.com>
2022-06-03 23:40:00 +0530

    chore: replace tx & address explorer concat with util function

--- pages/payouts.tsx
+++ pages/payouts.tsx
@@ -11,11 +11,11 @@ import {
 import { utils } from 'ethers'
 import router from 'next/router'
 import CopyIcon from 'src/components/ui/copy_icon'
-import { CHAIN_INFO } from 'src/constants/chains'
 import { SupportedChainId } from 'src/constants/chains'
 import { useGetFundSentforReviewerQuery } from 'src/generated/graphql'
 // TOOLS AND UTILS
 import {
+	getExplorerUrlForTxHash,
 	getFormattedDateFromUnixTimestampWithYear,
 	trimAddress,
 } from 'src/utils/formattingUtils'
@@ -283,12 +283,8 @@ export default function Payouts() {
 													<Flex direction="row">
 														<Link
 															href={
-																workspaceChainId ?
-																	`${CHAIN_INFO[workspaceChainId]
-																		.explorer.transactionHash}${data.id.substr(
-																		0,
-																		data.id.indexOf('.'),
-																	)}`
+																workspaceChainId
+																	? `${getExplorerUrlForTxHash(workspaceChainId, data.id.substr(0, data.id.indexOf('.')))}`
 																	: ''
 															}
 															isExternal
--- src/hooks/useDepositFunds.ts
+++ src/hooks/useDepositFunds.ts
@@ -2,8 +2,8 @@ import React, { useContext, useEffect } from 'react'
 import { ToastId, useToast } from '@chakra-ui/react'
 import { BigNumber } from 'ethers'
 import { ApiClientsContext } from 'pages/_app'
-import { CHAIN_INFO } from 'src/constants/chains'
 import getErrorMessage from 'src/utils/errorUtils'
+import { getExplorerUrlForTxHash } from 'src/utils/formattingUtils'
 import { getSupportedChainIdFromWorkspace } from 'src/utils/validationUtils'
 import { useAccount, useNetwork } from 'wagmi'
 import ErrorToast from '../components/ui/toasts/errorToast'
@@ -186,10 +186,7 @@ export default function useDepositFunds(
 
 	return [
 		transactionData,
-		currentChainId
-			? `${CHAIN_INFO[currentChainId]
-				.explorer.transactionHash}${transactionData?.transactionHash}`
-			: '',
+		getExplorerUrlForTxHash(currentChainId, transactionData?.transactionHash),
 		loading,
 	]
 }
