144ef33416a9dadb5d0f879d982fe8e3981984b4
Sourab <sourab@creatoros.co>
2022-07-12 01:40:05 +0530

    browse dao

---
+++ pages/browse_dao.tsx
@@ -0,0 +1,15 @@
+import { Box, Container, Text } from '@chakra-ui/react'
+
+function BrowseDao() {
+	return (
+		<Container>
+			<Box>
+				<Text>
+                    Browse DAO
+				</Text>
+			</Box>
+		</Container>
+	)
+}
+
+export default BrowseDao
---
+++ src/components/browse_daos/index.tsx
@@ -0,0 +1,13 @@
+import { Box, Text } from '@chakra-ui/react'
+
+function BrowseDao() {
+	return (
+		<Box>
+			<Text>
+                Browse DAO
+			</Text>
+		</Box>
+	)
+}
+
+export default BrowseDao
