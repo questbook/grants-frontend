ec322ec52f2bebf3d877fc9f2cb8719319bd8653
Adhiraj Singh <adhirajsingh1001@gmail.com>
2022-07-13 00:16:27 +0530

    refactor: cleaner contract usage

--- src/hooks/contracts/useQBContract.ts
+++ src/hooks/contracts/useQBContract.ts
@@ -3,36 +3,30 @@ import { SupportedChainId } from 'src/constants/chains'
 import { CHAIN_INFO } from 'src/constants/chains'
 import { QBContract, QBContractABIMap } from 'src/types'
 import { useContract, useSigner } from 'wagmi'
-import WorkspaceRegistryABI from '../../contracts/abi/WorkspaceRegistryAbi.json'
-import  GrantFactoryAbi  from '../../contracts/abi/GrantFactoryAbi.json'
-import  ApplicationRegistryAbi  from '../../contracts/abi/ApplicationRegistryAbi.json'
-import  ApplicationReviewRegistryAbi  from '../../contracts/abi/ApplicationReviewRegistryAbi.json'
-import { ContractInterface } from 'ethers'
+import ApplicationRegistryAbi from '../../contracts/abi/ApplicationRegistryAbi.json'
+import ApplicationReviewRegistryAbi from '../../contracts/abi/ApplicationReviewRegistryAbi.json'
+import GrantFactoryAbi from '../../contracts/abi/GrantFactoryAbi.json'
+import WorkspaceRegistryAbi from '../../contracts/abi/WorkspaceRegistryAbi.json'
 
 export default function useQBContract<C extends QBContract>(name: C, chainId?: SupportedChainId) {
 	const { data: signer } = useSigner()
-	let contractI: ContractInterface = WorkspaceRegistryABI
 	const addressOrName = useMemo(() => {
 		const address = CHAIN_INFO[chainId!]?.qbContracts?.[name]
 		return address || '0x0000000000000000000000000000000000000000'
 	}, [name, chainId])
 
-	if (name === "workspace") {
-		contractI = WorkspaceRegistryABI
-	} else if (name === "grantFactory") {
-		contractI = GrantFactoryAbi
-	} else if (name === "applications") {
-		contractI = ApplicationRegistryAbi
-	} else if (name === "reviews") {
-		contractI = ApplicationReviewRegistryAbi
-	}
-
-
 	const contract = useContract<QBContractABIMap[C]>({
 		addressOrName,
-		contractInterface: contractI,
+		contractInterface: CONTRACT_INTERFACE_MAP[name],
 		signerOrProvider: signer,
 	})
 
 	return contract
 }
+
+const CONTRACT_INTERFACE_MAP = {
+	workspace: WorkspaceRegistryAbi,
+	grantFactory: GrantFactoryAbi,
+	applications: ApplicationRegistryAbi,
+	reviews: ApplicationReviewRegistryAbi,
+} as const
