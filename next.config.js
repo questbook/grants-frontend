9c05a26faddd49be4e4de6092f1bb03bafface68
hasankhadra <59062695+hasankhadra@users.noreply.github.com>
2022-07-21 13:08:57 +0300

    add: CLIENT_ID for github oauth

--- next.config.js
+++ next.config.js
@@ -12,9 +12,9 @@ const nextConfig = {
 	},
 	env: {
 		NEXT_PUBLIC_IS_TEST: process.env.NEXT_PUBLIC_IS_TEST,
-		CLIENT_ID: process.env.CLIENT_ID,
-		CLIENT_SECRET: process.env.CLIENT_SECRET,
-		BICO_AUTH_TOKEN: process.env.BICO_AUTH_TOKEN
+		CLIENT_ID: "33757236b2fdb527f204"
+		// CLIENT_SECRET: process.env.CLIENT_SECRET,
+		// BICO_AUTH_TOKEN: process.env.BICO_AUTH_TOKEN
 	},
 	async rewrites() {
         return [
