b205ab535d7b8be1c3c7a10ffddda3ad1b3b0899
Mohammad Shahin <59062739+MohammadShahin@users.noreply.github.com>
2022-08-01 16:22:45 +0300

    Fixed styles in your_grant/view_applications

--- src/components/your_grants/grantStatsBox.tsx
+++ src/components/your_grants/grantStatsBox.tsx
@@ -2,42 +2,48 @@ import React from 'react'
 import { Box, Flex, Image, Text, VStack } from '@chakra-ui/react'
 
 interface Props {
-  numberOfApplicants: number;
-  totalDisbursed: number;
+	numberOfApplicants: number;
+	totalDisbursed: number;
+	numberOfReviews: number;
 }
 
-function GrantStatsBox({ numberOfApplicants, totalDisbursed }: Props) {
+function GrantStatsBox({ numberOfApplicants, totalDisbursed, numberOfReviews }: Props) {
 
 	return (
 		<Flex
 			direction="column"
-			mb="10px"
+			mb="10"
+			maxW='100%'
 		>
 			<Box
 				box-sizing="border-box"
 				border="1px solid #D2D2E3"
 				borderRadius="4px"
-
+				maxW='100%'
 			>
-				<Flex>
+				<Flex maxW='100%'>
 					<Box
 						flex="1"
 						border="1px solid #D2D2E3"
-						h="152px">
+						h="152px"
+					>
 						<VStack
-							align="center"
-							spacing={4}>
+							align="left"
+							spacing={4}
+							mt={5}
+							ml={5}
+						>
 							<Image
 								src="/ui_icons/applicants_total.svg"
 								h="24.5px"
 								w="19px" />
 
-							<Text>
+							<Text variant="ApplicantsStatsMajor">
 								{numberOfApplicants}
 							</Text>
 
-							<Text>
-Total Applicants
+							<Text variant="ApplicantsStatsMinor">
+								Total Applicants
 							</Text>
 						</VStack>
 					</Box>
@@ -46,18 +52,20 @@ Total Applicants
 						border="1px solid #D2D2E3"
 						h="152px">
 						<VStack
-							align="center"
-							spacing={4}>
+							align="left"
+							spacing={4}
+							mt={5}
+							ml={5}>
 							<Image
 								src="/ui_icons/review.svg"
 								h="24.5px"
 								w="19px" />
 
-							<Text>
-0
+							<Text variant="ApplicantsStatsMajor">
+								{numberOfReviews}
 							</Text>
-							<Text>
-Reviews
+							<Text variant="ApplicantsStatsMinor">
+								Reviews
 							</Text>
 						</VStack>
 					</Box>
@@ -66,19 +74,22 @@ Reviews
 						border="1px solid #D2D2E3"
 						h="152px">
 						<VStack
-							align="center"
-							spacing={4}>
+							align="left"
+							spacing={4}
+							mt={5}
+							ml={5}
+						>
 							<Image
 								src="/ui_icons/dollar_icon.svg"
 								h="24.5px"
 								w="19px" />
 
-							<Text>
-								{totalDisbursed}
+							<Text variant="ApplicantsStatsMajor">
+								${totalDisbursed}
 							</Text>
 
-							<Text>
-Disbursed
+							<Text variant="ApplicantsStatsMinor">
+								Disbursed
 							</Text>
 						</VStack>
 					</Box>
--- src/theme/components/text.tsx
+++ src/theme/components/text.tsx
@@ -40,6 +40,18 @@ export default {
 				color: 'brand.500',
 				fontWeight: '700',
 			},
+			ApplicantsStatsMajor: {
+				fontSize: '26px',
+				fontWeight: '500',
+				lineHeight: '32px',
+				color: '#1F1F33'
+			},
+			ApplicantsStatsMinor: {
+				fontSize: '12px',
+				fontWeight: '400',
+				lineHeight: '16px',
+				color: '#7D7DA0'
+			},
 			tableBody: {
 				fontSize: '16px',
 				lineHeight: '24px',
