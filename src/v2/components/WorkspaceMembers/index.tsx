9299d7263eeb6b949723b9f758f6fb6a71b6bdf8
Rithik Bhandari <rithikbhandari895@gmail.com>
2022-08-01 19:15:59 +0530

    feat: added WorkspaceMembers component

---
+++ src/v2/components/WorkspaceMembers/index.tsx
@@ -0,0 +1,244 @@
+import React, { useContext, useEffect, useState } from 'react'
+import { ChevronLeftIcon, ChevronRightIcon, LinkIcon } from '@chakra-ui/icons'
+import {
+	Box,
+	Button,
+	Center,
+	Checkbox,
+	Flex,
+	Grid,
+	GridItem,
+	Spacer,
+	Table,
+	Tag,
+	Tbody,
+	Td,
+	Th,
+	Thead,
+	Tr,
+	Wrap,
+	WrapItem,
+} from '@chakra-ui/react'
+import { capitalizeFirstLetter } from 'src/utils/generics'
+import { ApiClientsContext } from '../../../../pages/_app'
+import Loader from '../../../components/ui/loader'
+import { defaultChainId } from '../../../constants/chains'
+import { useGetWorkspaceMembersByWorkspaceIdQuery, WorkspaceMemberAccessLevel } from '../../../generated/graphql'
+import { getFormattedDateFromUnixTimestampWithYear } from '../../../utils/formattingUtils'
+import { getSupportedChainIdFromWorkspace } from '../../../utils/validationUtils'
+import InviteModal from '../InviteModal'
+
+const PAGE_SIZE = 7
+
+const userTypes = [
+	{
+		'name': 'All',
+		'accessLevels': Object.values(WorkspaceMemberAccessLevel),
+	},
+	{
+		'name': 'Administrators',
+		'accessLevels': [WorkspaceMemberAccessLevel['Admin']],
+	},
+	{
+		'name': 'Reviewers',
+		'accessLevels': [WorkspaceMemberAccessLevel['Reviewer']],
+	},
+]
+
+
+const tableHeaders = ['', 'Member', 'Role', 'Joined on']
+
+function WorkspaceMembers() {
+	const { workspace, subgraphClients } = useContext(ApiClientsContext)!
+
+	const [selectedUserTypeIdx, setSelectedUserTypeIdx] = useState(0)
+
+	const [members, setMembers] = useState<Array<any> | undefined>(undefined)
+
+	const [page, setPage] = useState(0)
+	const [hasMoreData, setHasMoreData] = useState(true)
+
+	const { data } = useGetWorkspaceMembersByWorkspaceIdQuery({
+		client:
+    subgraphClients[
+    	getSupportedChainIdFromWorkspace(workspace) || defaultChainId
+    ].client,
+		variables: {
+			workspaceId: workspace!.id,
+			first: PAGE_SIZE,
+			skip: page * PAGE_SIZE,
+			accessLevelsIn: userTypes[selectedUserTypeIdx].accessLevels,
+		},
+	})
+
+	useEffect(() => {
+		if(!data) {
+			return
+		}
+
+		setHasMoreData(data!.workspaceMembers.length >= PAGE_SIZE)
+		setMembers(data!.workspaceMembers)
+	}, [data, page, selectedUserTypeIdx])
+
+	const [isInviteModalOpen, setIsInviteModalOpen] = React.useState(false)
+
+	return (
+		<>
+			<Flex
+				direction={'column'}
+			>
+				<Flex
+					direction={'row'}
+				>
+					<Wrap>
+						{
+							userTypes.map((userType, index) => (
+								<WrapItem key={index}>
+									<Tag
+										onClick={
+											() => {
+												setPage(0)
+												setSelectedUserTypeIdx(index)
+											}
+										}
+										bg={index === selectedUserTypeIdx ? '#1F1F32' : '#E0E0EC'}
+										fontSize={18}
+										borderRadius={2}
+										paddingTop={1}
+										paddingBottom={1}
+										letterSpacing={-1}
+										textTransform={'none'}
+										cursor={'pointer'}
+										color={index === selectedUserTypeIdx ? '#E0E0EC' : '#1F1F32'}>
+										{userType.name}
+									</Tag>
+								</WrapItem>
+							),
+							)
+						}
+
+					</Wrap>
+					<Spacer />
+					<Button
+						onClick={() => setIsInviteModalOpen(true)}
+						leftIcon={<LinkIcon />}
+						bg={'#1F1F32'}
+						colorScheme={'#E0E0EC'}
+						borderRadius={4}
+						variant='solid'>
+            Create invite link
+					</Button>
+				</Flex>
+				<Box h={5} />
+				<Box
+					boxShadow={'lg'}
+					borderRadius={7.5}
+					bg={'white'}
+				>
+					<Table>
+						<Thead>
+							<Tr>
+								{
+									tableHeaders.map((tableHeader) => (
+										<Th
+											key={tableHeader}
+											fontSize={18}
+											fontWeight={'bold'}
+											letterSpacing={-1}
+											textTransform={'none'}
+										>
+											{tableHeader}
+										</Th>
+									))
+								}
+							</Tr>
+						</Thead>
+						{
+							members === undefined ? (
+								<Center>
+									<Loader />
+								</Center>
+							) : members!.length === 0 ? (
+								<Center>
+									<span>
+										{`No ${page === 0 ? '' : 'more '}members!`}
+									</span>
+								</Center>
+							)
+								:
+								(
+									<Tbody>
+										{
+                      members!.map((member: any) => (
+                      	<Tr key={member.id}>
+                      		<Td>
+                      			<Checkbox />
+                      		</Td>
+                      		<Td>
+                      			<Grid>
+                      				<GridItem fontWeight={'bold'}>
+                      					{member.fullName}
+                      				</GridItem>
+                      				<GridItem color={'#9292AF'}>
+                      					{member.actorId}
+                      				</GridItem>
+                      			</Grid>
+                      		</Td>
+                      		<Td>
+                      			<Tag
+                      				bg={'#FFC403'}
+                      				borderRadius={2}
+                      				fontWeight={'bold'}
+                      			>
+                      				{capitalizeFirstLetter(member.accessLevel)}
+                      			</Tag>
+                      		</Td>
+                      		<Td>
+                      			{getFormattedDateFromUnixTimestampWithYear(member.addedAt)}
+                      		</Td>
+                      	</Tr>
+                      ))
+										}
+									</Tbody>
+								)
+						}
+					</Table>
+				</Box>
+				<Box h={2} />
+				<Flex
+					justifyContent={'end'}>
+					<Box
+						boxShadow={'lg'}
+						borderRadius={7.5}
+						bg={'white'}>
+						<Flex
+							alignItems={'center'}>
+							<Button
+								bg={'inherit'}
+								disabled={page === 0}
+								onClick={() => setPage((page) => page - 1)}
+							>
+								<ChevronLeftIcon />
+							</Button>
+							<Box w={2.5} />
+							{page + 1}
+							<Box w={2.5} />
+							<Button
+								bg={'inherit'}
+								disabled={!hasMoreData}
+								onClick={() => setPage((page) => page + 1)}
+							>
+								<ChevronRightIcon />
+							</Button>
+						</Flex>
+					</Box>
+				</Flex>
+			</Flex>
+			<InviteModal
+				isOpen={isInviteModalOpen}
+				onClose={() => setIsInviteModalOpen(false)} />
+		</>
+	)
+}
+
+export default WorkspaceMembers
