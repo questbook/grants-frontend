84c52ba9d69ebb92e244a4b6f9407354871989cf
Rithik Bhandari <rithikbhandari895@gmail.com>
2022-08-09 17:15:37 +0530

    style: updated reviewer dashboard UI

--- src/v2/components/ReviewerDashboard/index.tsx
+++ src/v2/components/ReviewerDashboard/index.tsx
@@ -1,9 +1,12 @@
-import { Box, Flex } from '@chakra-ui/react'
+import { useState } from 'react'
+import { Box, Button, Flex } from '@chakra-ui/react'
 import { useAccount } from 'wagmi'
 import { ApplicationState } from '../../../generated/graphql'
 import ApplicationsTable from './ApplicationsTable'
 
 function ReviewerDashboard() {
+	const [showPendingReviews, setShowPendingReviews] = useState(true)
+
 	const { data: accountData } = useAccount()
 
 	const reviewDoneStates = Object.values(ApplicationState)
@@ -15,20 +18,35 @@ function ReviewerDashboard() {
 		<Flex
 			direction={'column'}>
 			<Box h={30} />
+			<Box
+				bg={'#f0f0f7'}
+				padding={'5px'}>
+				<Flex gap={'5px'}>
+					<Button
+						bg={showPendingReviews ? 'white' : 'transparent'}
+						color={showPendingReviews ? 'black' : '#7D7DA0'}
+						onClick={() => setShowPendingReviews(true)}
+						borderRadius={'5px'}
+						padding={'10px'}>
+            Reviews - waiting on you
+					</Button>
+					<Button
+						bg={showPendingReviews ? 'transparent' : 'white'}
+						color={showPendingReviews ? '#7D7DA0' : 'black'}
+						onClick={() => setShowPendingReviews(false)}
+						borderRadius={'5px'}
+						padding={'10px'}>
+            Reviews done
+					</Button>
+				</Flex>
+			</Box>
+			<Box h={5} />
 			<ApplicationsTable
 				reviewerId={accountData!.address!}
-				showApplicationState={false}
-				showReviewButton={true}
-				header={'Reviews - waiting on you'}
-				applicationStateIn={[ApplicationState.Submitted]} />
-			<Box h={10} />
-			<ApplicationsTable
-				reviewerId={accountData!.address!}
-				showApplicationState={true}
-				showReviewButton={false}
-				header={'Reviews done'}
-				applicationStateIn={reviewDoneStates} />
-			<Box h={10} />
+				showApplicationState={!showPendingReviews}
+				showReviewButton={showPendingReviews}
+				applicationStateIn={showPendingReviews ? [ApplicationState.Submitted] : reviewDoneStates}
+			/>
 		</Flex>
 	)
 }
