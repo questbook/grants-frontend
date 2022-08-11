adc4aa7ae62208fa7ab322e90c8866d8dc8d1e96
Mohammad Shahin <59062739+MohammadShahin@users.noreply.github.com>
2022-08-11 17:08:05 +0300

    lint

--- pages/onboarding/create-domain.tsx
+++ pages/onboarding/create-domain.tsx
@@ -1,12 +1,12 @@
 import { useEffect, useState } from 'react'
 import { Box, Flex, HStack, Image, Spacer, Text } from '@chakra-ui/react'
+import useSafeUSDBalances from 'src/hooks/useSafeUSDBalances'
 import { Organization } from 'src/v2/assets/custom chakra icons/Organization'
 import AccountDetails from 'src/v2/components/NavBar/AccountDetails'
 import NetworkTransactionModal from 'src/v2/components/NetworkTransactionModal'
 import { ConfirmData, DomainName, SafeAddress } from 'src/v2/components/Onboarding/CreateDomain'
 import QuestbookLogo from 'src/v2/components/QuestbookLogo'
 import VerifySignerModal from 'src/v2/components/VerifySignerModal'
-import useSafeUSDBalances from 'src/hooks/useSafeUSDBalances'
 
 const OnboardingCreateDomain = () => {
 	const [step, setStep] = useState(0)
@@ -17,7 +17,7 @@ const OnboardingCreateDomain = () => {
 	const [isSafeAddressPasted, setIsSafeAddressPasted] = useState(false)
 	const [isSafeAddressVerified, setIsSafeAddressVerified] = useState(false)
 	const [isSafesLoading, setIsSafesLoading] = useState(true)
-	const { data: safesUSDBalance } = useSafeUSDBalances( { safeAddress } )
+	const { data: safesUSDBalance } = useSafeUSDBalances({ safeAddress })
 
 	// State variables for step 2
 	const [domainName, setDomainName] = useState('')
--- src/hooks/useSafeUSDBalances.ts
+++ src/hooks/useSafeUSDBalances.ts
@@ -24,32 +24,36 @@ interface SingleTokenData {
 interface AllTokensData extends Array<SingleTokenData> { }
 
 function useSafeUSDBalances({ safeAddress }: Props) {
-    const urls = useMemo(() => {
-        if (safeAddress === '') return []
-        return SAFES_BALANCES_ENPOINTS.map(element => element + URL_PREFIX + safeAddress + URL_SUFFIX)
-    }, [safeAddress])
+	const urls = useMemo(() => {
+		if(safeAddress === '') {
+			return []
+		}
 
-    const { data: rawData, error, loaded } = useAxiosMulti({
-        urls,
-        method: 'get'
-    })
-    const [data, setData] = useState<USDBalancesData>({})
+		return SAFES_BALANCES_ENPOINTS.map(element => element + URL_PREFIX + safeAddress + URL_SUFFIX)
+	}, [safeAddress])
 
-    useEffect(() => {
-        if (loaded && !error) {
-            const newData: USDBalancesData = {}
-            rawData.forEach((allTokensData: AllTokensData, index) => {
-                const currentChainID = SAFES_BALANCES_CHAIN_ID[index]
-                let tokensSum = allTokensData.reduce((partialSum: number, item) => partialSum + parseFloat(item?.fiatBalance || '0'), 0)
-                if(tokensSum >= USD_BALANCE_THRESHOLD)
-                    newData[currentChainID] = tokensSum
-            })
-            console.log('Final Safe', newData)
-            setData(newData)
-        }
-    }, [rawData, loaded, error])
+	const { data: rawData, error, loaded } = useAxiosMulti({
+		urls,
+		method: 'get'
+	})
+	const [data, setData] = useState<USDBalancesData>({})
 
-    return { error, loaded, data }
+	useEffect(() => {
+		if(loaded && !error) {
+			const newData: USDBalancesData = {}
+			rawData.forEach((allTokensData: AllTokensData, index) => {
+				const currentChainID = SAFES_BALANCES_CHAIN_ID[index]
+				const tokensSum = allTokensData.reduce((partialSum: number, item) => partialSum + parseFloat(item?.fiatBalance || '0'), 0)
+				if(tokensSum >= USD_BALANCE_THRESHOLD) {
+					newData[currentChainID] = tokensSum
+				}
+			})
+			console.log('Final Safe', newData)
+			setData(newData)
+		}
+	}, [rawData, loaded, error])
+
+	return { error, loaded, data }
 }
 
 export default useSafeUSDBalances
