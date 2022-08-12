bc8867c06f321883356a7f2eff5bd7641b298bf6
hasankhadra <59062695+hasankhadra@users.noreply.github.com>
2022-08-12 13:55:48 +0300

    removed privateKey

--- pages/signup_webwallet.tsx
+++ pages/signup_webwallet.tsx
@@ -27,13 +27,6 @@ function SignupWebwallet() {
 		// const apiKK = await addDapp("goerli-testing-dapp", '5', process.env.BICO_AUTH_TOKEN);    
 		// registerWebHook(process.env.BICO_AUTH_TOKEN!, "qypYydNmh.85fb44d4-bc3a-4434-8e51-a929f54de521");
 
-		const wallet = new ethers.Wallet("e1463f0cce09d1c645ae6ea5afdb56b0302b42a62d2e8d4192743a9f7d5b8d01", jsonRpcProviders["5"])
-		const contract = new ethers.Contract("0xf08bCcf3C9DE7Daf25BCe13C87D0dd69754E0a33", 
-		GrantFactoryAbi, wallet);
-		await contract.createGrant("1", "bafkreid4iqralm4cxstxkv5jgy763xit7gfyla3wqimobn675x72cr6ii4",
-		"bafkreiewmydvuser5nribk7qcpfjhxb35wajqonbgaxq47o36ligo6ysvm", "0x2dB223158288B2299480aF577eDF30D5a533F137",
-		"0xF4Db8BdDF1029764e4E09e7cE34149371a9A7027");
-		console.log("zob contract", contract);
 	}
 
 
