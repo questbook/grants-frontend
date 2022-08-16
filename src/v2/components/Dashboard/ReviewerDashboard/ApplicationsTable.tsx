aba191c4fa7d965b3f1082321cda6ab6e18d1f42
Rithik Bhandari <rithikbhandari895@gmail.com>
2022-08-16 19:59:57 +0530

    feat: added initialApplications for ApplicationsTable

--- src/v2/components/Dashboard/ReviewerDashboard/ApplicationsTable.tsx
+++ src/v2/components/Dashboard/ReviewerDashboard/ApplicationsTable.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useMemo, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { Box, Button, Center, Flex, Grid, GridItem, Table, Tbody, Td, Text, Th, Thead, Tr } from '@chakra-ui/react'
 import { useRouter } from 'next/router'
 import { useAccount } from 'wagmi'
@@ -6,10 +6,9 @@ import { ApiClientsContext } from '../../../../../pages/_app'
 import Loader from '../../../../components/ui/loader'
 import { CHAIN_INFO, defaultChainId } from '../../../../constants/chains'
 import {
-	ApplicationState,
-	GetReviewerApplicationsForGrantQuery,
-	useGetReviewerApplicationsForGrantQuery,
+	ApplicationState, GetInitialToBeReviewedApplicationGrantsQuery,
 } from '../../../../generated/graphql'
+import SupportedChainId from '../../../../generated/SupportedChainId'
 import useEncryption from '../../../../hooks/utils/useEncryption'
 import { formatAmount, getFormattedDateFromUnixTimestampWithYear } from '../../../../utils/formattingUtils'
 import { capitalizeFirstLetter } from '../../../../utils/generics'
@@ -36,9 +35,10 @@ const STATUS_COLORS: { [key in ApplicationState]?: { 'text': string, 'bg': strin
 	},
 }
 
-type ReviewType = GetReviewerApplicationsForGrantQuery['grantApplications'][0]['reviews'][0]
+type InitialApplicationType = GetInitialToBeReviewedApplicationGrantsQuery['grantReviewerCounters'][0]['grant']['applications'][0]
+type ReviewType = InitialApplicationType['reviews'][0]
 
-const PAGE_SIZE = 5
+export const APPLICATIONS_TABLE_PAGE_SIZE = 5
 
 type Application = {
   id: string,
@@ -56,96 +56,120 @@ type Application = {
 type Props = {
   reviewerId: string,
   grantId: string,
-  showApplicationState: boolean,
-  applicationStateIn: ApplicationState[],
+  initialApplications?: InitialApplicationType[],
+  showToBeReviewedApplications: boolean,
+}
+
+const parseApplication = (application: InitialApplicationType, chainId: SupportedChainId): Application => {
+	const decimals = CHAIN_INFO[
+		getSupportedChainIdFromSupportedNetwork(
+			application.grant.workspace.supportedNetworks[0],
+		)
+	]?.supportedCurrencies[application.grant.reward.asset.toLowerCase()]
+		?.decimals || 18
+
+	const getFieldString = (name: string) => application.fields.find((field) => field?.id?.includes(`.${name}`))?.values[0]?.value
+
+	let rewardAmount: number
+
+	const fundingAskField = getFieldString('fundingAsk')
+	if(fundingAskField) {
+		rewardAmount = +formatAmount(
+      getFieldString('fundingAsk')!,
+      decimals,
+		)
+	} else {
+		rewardAmount = 0
+		application.milestones.forEach(
+			(milestone) => rewardAmount += +formatAmount(
+				milestone.amount,
+				decimals,
+			))
+	}
+
+	const tokenLabel = getAssetInfo(
+		application?.grant?.reward?.asset?.toLowerCase(),
+		chainId,
+	).label
+
+	return {
+		id: application.id,
+		submittedOn: application.createdAtS,
+		reviews: application.reviews,
+		grantRubricIsPrivate: application.grant.rubric?.isPrivate,
+		reward: `${rewardAmount} ${tokenLabel}`,
+		state: application.state,
+		applicantId: application.applicantId,
+		projectName: getFieldString('projectName')!,
+		applicantEmail: getFieldString('applicantEmail'),
+		applicantName: getFieldString('applicantName')!,
+	}
 }
 
 
 function ApplicationsTable({
 	reviewerId,
 	grantId,
-	applicationStateIn,
-	showApplicationState,
+	showToBeReviewedApplications,
+	initialApplications,
 }: Props) {
 	const [page, setPage] = useState(0)
+	const [applications, setApplications] = useState<Array<Application>>()
 	const [hasMoreData, setHasMoreData] = useState(true)
 
 	const { workspace, subgraphClients } = useContext(ApiClientsContext)!
 
 	const chainId = getSupportedChainIdFromWorkspace(workspace) || defaultChainId
-	const { client } = subgraphClients[chainId]
+	// const { client } = subgraphClients[chainId]
 
-	const { data } = useGetReviewerApplicationsForGrantQuery({
-		client,
-		variables: {
-			workspaceId: workspace!.id,
-			reviewerId,
-			grantId,
-			applicationStateIn,
-			first: PAGE_SIZE,
-			skip: page * PAGE_SIZE,
-		},
-	})
-
-	const applications = useMemo(() => {
-		if(!data) {
-			return
+	useEffect(() => {
+		if(applications) {
+			setHasMoreData(applications!.length >= APPLICATIONS_TABLE_PAGE_SIZE)
+		} else {
+			setHasMoreData(false)
 		}
+	}, [applications])
 
-		setHasMoreData(data!.grantApplications.length >= PAGE_SIZE)
+	useEffect(() => {
+		if(initialApplications !== undefined) {
+			setApplications(initialApplications.map(
+				(application) => (parseApplication(application, chainId)),
+			))
+		}
+	}, [])
 
-		const fetchedApplications: Application[] = data!.grantApplications.map((application) => {
-			const decimals = CHAIN_INFO[
-				getSupportedChainIdFromSupportedNetwork(
-					application.grant.workspace.supportedNetworks[0],
-				)
-			]?.supportedCurrencies[application.grant.reward.asset.toLowerCase()]
-				?.decimals || 18
 
-			const getFieldString = (name: string) => application.fields.find((field) => field?.id?.includes(`.${name}`))?.values[0]?.value
-
-			let rewardAmount: number
-
-			const fundingAskField = getFieldString('fundingAsk')
-			if(fundingAskField) {
-				rewardAmount = +formatAmount(
-          getFieldString('fundingAsk')!,
-          decimals,
-				)
-			} else {
-				rewardAmount = 0
-				application.milestones.forEach(
-					(milestone) => rewardAmount += +formatAmount(
-						milestone.amount,
-						decimals,
-					))
-			}
-
-			const tokenLabel = getAssetInfo(
-				application?.grant?.reward?.asset?.toLowerCase(),
-				getSupportedChainIdFromWorkspace(workspace),
-			).label
-
-			return {
-				id: application.id,
-				submittedOn: application.createdAtS,
-				reviews: application.reviews,
-				grantRubricIsPrivate: application.grant.rubric?.isPrivate,
-				reward: `${rewardAmount} ${tokenLabel}`,
-				state: application.state,
-				applicantId: application.applicantId,
-				projectName: getFieldString('projectName')!,
-				applicantEmail: getFieldString('applicantEmail'),
-				applicantName: getFieldString('applicantName')!,
-			}
-		})
-
-		return fetchedApplications
-	}, [page, data])
+	//
+	// const variables = {
+	// 	reviewerId: reviewerId.toLowerCase(),
+	// 	grantId,
+	// 	first: APPLICATIONS_TABLE_PAGE_SIZE,
+	// 	skip: page * APPLICATIONS_TABLE_PAGE_SIZE,
+	// }
+	// const { data } = showToBeReviewedApplications ?
+	// 	useGetToBeReviewedReviewerApplicationsForGrantQuery({
+	// 		client, variables: { ...variables, doesNotHaveReviewOfReviewerId: reviewerId },
+	// 	}) : useGetDoneReviewerApplicationsForGrantQuery({
+	// 		client, variables: { ...variables, shouldHaveReviewOfReviewerId: reviewerId },
+	// 	})
+	//
+	// const applications = useMemo(() => {
+	// 	if(!data) {
+	// 		return
+	// 	}
+	//
+	// 	setHasMoreData(data!.grantApplications.length >= PAGE_SIZE)
+	//
+	// 	const fetchedApplications: Application[] = data!.grantApplications.map((application) => {
+	//
+	// 	})
+	//
+	// 	return fetchedApplications
+	// }, [page, data])
 
 	const TABLE_HEADERS = ['Project Name', 'Funding ask', 'Submitted on']
 
-	if(showApplicationState && !TABLE_HEADERS.includes('Status')) {
+	if(!showToBeReviewedApplications && !TABLE_HEADERS.includes('Status')) {
 		TABLE_HEADERS.push('Status')
 	}
 
@@ -201,7 +225,7 @@ function ApplicationsTable({
 										{getFormattedDateFromUnixTimestampWithYear(application.submittedOn)}
 									</Td>
 									{
-										showApplicationState && (
+										!showToBeReviewedApplications && (
 											<Td>
 												<Flex alignItems={'start'}>
 													<Text
