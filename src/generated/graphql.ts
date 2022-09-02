587eabf9d86f9815be169a32cb5fbc783e82e308
Adhiraj Singh <adhirajsingh1001@gmail.com>
2022-09-02 14:50:46 +0530

    feat: add getDAOsForExplore

--- src/generated/graphql.ts
+++ src/generated/graphql.ts
@@ -3509,6 +3509,12 @@ export type Workspace = {
   members: Array<WorkspaceMember>;
   /** Hash of the IPFS file from which the details about the workspace were pulled */
   metadataHash: Scalars['String'];
+  /** timestamp of when the latest grant was posted */
+  mostRecentGrantPostedAtS: Scalars['Int'];
+  /** total number of applications the DAO has received */
+  numberOfApplications: Scalars['Int'];
+  /** total number of applications the DAO has accepted */
+  numberOfApplicationsSelected: Scalars['Int'];
   /** Address of the owner of the workspace */
   ownerId: Scalars['Bytes'];
   partners: Array<Partner>;
@@ -3522,6 +3528,10 @@ export type Workspace = {
   title: Scalars['String'];
   /** Custom tokens setup by the workspace */
   tokens: Array<Token>;
+  /** total grant funding committed in USD */
+  totalGrantFundingCommittedUSD: Scalars['Int'];
+  /** total grant funding committed in USD */
+  totalGrantFundingDisbursedUSD: Scalars['Int'];
   /** in seconds since epoch */
   updatedAtS: Scalars['Int'];
 };
@@ -4012,6 +4022,30 @@ export type Workspace_Filter = {
   metadataHash_not_starts_with_nocase?: InputMaybe<Scalars['String']>;
   metadataHash_starts_with?: InputMaybe<Scalars['String']>;
   metadataHash_starts_with_nocase?: InputMaybe<Scalars['String']>;
+  mostRecentGrantPostedAtS?: InputMaybe<Scalars['Int']>;
+  mostRecentGrantPostedAtS_gt?: InputMaybe<Scalars['Int']>;
+  mostRecentGrantPostedAtS_gte?: InputMaybe<Scalars['Int']>;
+  mostRecentGrantPostedAtS_in?: InputMaybe<Array<Scalars['Int']>>;
+  mostRecentGrantPostedAtS_lt?: InputMaybe<Scalars['Int']>;
+  mostRecentGrantPostedAtS_lte?: InputMaybe<Scalars['Int']>;
+  mostRecentGrantPostedAtS_not?: InputMaybe<Scalars['Int']>;
+  mostRecentGrantPostedAtS_not_in?: InputMaybe<Array<Scalars['Int']>>;
+  numberOfApplications?: InputMaybe<Scalars['Int']>;
+  numberOfApplicationsSelected?: InputMaybe<Scalars['Int']>;
+  numberOfApplicationsSelected_gt?: InputMaybe<Scalars['Int']>;
+  numberOfApplicationsSelected_gte?: InputMaybe<Scalars['Int']>;
+  numberOfApplicationsSelected_in?: InputMaybe<Array<Scalars['Int']>>;
+  numberOfApplicationsSelected_lt?: InputMaybe<Scalars['Int']>;
+  numberOfApplicationsSelected_lte?: InputMaybe<Scalars['Int']>;
+  numberOfApplicationsSelected_not?: InputMaybe<Scalars['Int']>;
+  numberOfApplicationsSelected_not_in?: InputMaybe<Array<Scalars['Int']>>;
+  numberOfApplications_gt?: InputMaybe<Scalars['Int']>;
+  numberOfApplications_gte?: InputMaybe<Scalars['Int']>;
+  numberOfApplications_in?: InputMaybe<Array<Scalars['Int']>>;
+  numberOfApplications_lt?: InputMaybe<Scalars['Int']>;
+  numberOfApplications_lte?: InputMaybe<Scalars['Int']>;
+  numberOfApplications_not?: InputMaybe<Scalars['Int']>;
+  numberOfApplications_not_in?: InputMaybe<Array<Scalars['Int']>>;
   ownerId?: InputMaybe<Scalars['Bytes']>;
   ownerId_contains?: InputMaybe<Scalars['Bytes']>;
   ownerId_in?: InputMaybe<Array<Scalars['Bytes']>>;
@@ -4060,6 +4094,22 @@ export type Workspace_Filter = {
   title_starts_with?: InputMaybe<Scalars['String']>;
   title_starts_with_nocase?: InputMaybe<Scalars['String']>;
   tokens_?: InputMaybe<Token_Filter>;
+  totalGrantFundingCommittedUSD?: InputMaybe<Scalars['Int']>;
+  totalGrantFundingCommittedUSD_gt?: InputMaybe<Scalars['Int']>;
+  totalGrantFundingCommittedUSD_gte?: InputMaybe<Scalars['Int']>;
+  totalGrantFundingCommittedUSD_in?: InputMaybe<Array<Scalars['Int']>>;
+  totalGrantFundingCommittedUSD_lt?: InputMaybe<Scalars['Int']>;
+  totalGrantFundingCommittedUSD_lte?: InputMaybe<Scalars['Int']>;
+  totalGrantFundingCommittedUSD_not?: InputMaybe<Scalars['Int']>;
+  totalGrantFundingCommittedUSD_not_in?: InputMaybe<Array<Scalars['Int']>>;
+  totalGrantFundingDisbursedUSD?: InputMaybe<Scalars['Int']>;
+  totalGrantFundingDisbursedUSD_gt?: InputMaybe<Scalars['Int']>;
+  totalGrantFundingDisbursedUSD_gte?: InputMaybe<Scalars['Int']>;
+  totalGrantFundingDisbursedUSD_in?: InputMaybe<Array<Scalars['Int']>>;
+  totalGrantFundingDisbursedUSD_lt?: InputMaybe<Scalars['Int']>;
+  totalGrantFundingDisbursedUSD_lte?: InputMaybe<Scalars['Int']>;
+  totalGrantFundingDisbursedUSD_not?: InputMaybe<Scalars['Int']>;
+  totalGrantFundingDisbursedUSD_not_in?: InputMaybe<Array<Scalars['Int']>>;
   updatedAtS?: InputMaybe<Scalars['Int']>;
   updatedAtS_gt?: InputMaybe<Scalars['Int']>;
   updatedAtS_gte?: InputMaybe<Scalars['Int']>;
@@ -4079,6 +4129,9 @@ export enum Workspace_OrderBy {
   LogoIpfsHash = 'logoIpfsHash',
   Members = 'members',
   MetadataHash = 'metadataHash',
+  MostRecentGrantPostedAtS = 'mostRecentGrantPostedAtS',
+  NumberOfApplications = 'numberOfApplications',
+  NumberOfApplicationsSelected = 'numberOfApplicationsSelected',
   OwnerId = 'ownerId',
   Partners = 'partners',
   Safe = 'safe',
@@ -4086,6 +4139,8 @@ export enum Workspace_OrderBy {
   SupportedNetworks = 'supportedNetworks',
   Title = 'title',
   Tokens = 'tokens',
+  TotalGrantFundingCommittedUsd = 'totalGrantFundingCommittedUSD',
+  TotalGrantFundingDisbursedUsd = 'totalGrantFundingDisbursedUSD',
   UpdatedAtS = 'updatedAtS'
 }
 
@@ -4242,6 +4297,16 @@ export type GetDaoNameQueryVariables = Exact<{
 
 
 export type GetDaoNameQuery = { __typename?: 'Query', workspace?: { __typename?: 'Workspace', id: string, title: string, about: string, logoIpfsHash: string, coverImageIpfsHash?: string | null } | null };
+
+export type GetDaOsForExploreQueryVariables = Exact<{
+  first?: InputMaybe<Scalars['Int']>;
+  skip?: InputMaybe<Scalars['Int']>;
+  orderBy: Workspace_OrderBy;
+  filter: Workspace_Filter;
+}>;
+
+
+export type GetDaOsForExploreQuery = { __typename?: 'Query', workspaces: Array<{ __typename?: 'Workspace', id: string, title: string, logoIpfsHash: string, supportedNetworks: Array<SupportedNetwork>, createdAtS: number, mostRecentGrantPostedAtS: number, numberOfApplications: number, numberOfApplicationsSelected: number, totalGrantFundingDisbursedUSD: number }> };
 
 export type GetFundSentforReviewerQueryVariables = Exact<{
   type?: InputMaybe<FundsTransferType>;
@@ -5486,6 +5551,58 @@ export function useGetDaoNameLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions
 export type GetDaoNameQueryHookResult = ReturnType<typeof useGetDaoNameQuery>;
 export type GetDaoNameLazyQueryHookResult = ReturnType<typeof useGetDaoNameLazyQuery>;
 export type GetDaoNameQueryResult = Apollo.QueryResult<GetDaoNameQuery, GetDaoNameQueryVariables>;
+export const GetDaOsForExploreDocument = gql`
+    query getDAOsForExplore($first: Int, $skip: Int, $orderBy: Workspace_orderBy!, $filter: Workspace_filter!) {
+  workspaces(
+    first: $first
+    skip: $skip
+    orderBy: $orderBy
+    orderDirection: desc
+    where: $filter
+  ) {
+    id
+    title
+    logoIpfsHash
+    supportedNetworks
+    createdAtS
+    mostRecentGrantPostedAtS
+    numberOfApplications
+    numberOfApplicationsSelected
+    totalGrantFundingDisbursedUSD
+  }
+}
+    `;
+
+/**
+ * __useGetDaOsForExploreQuery__
+ *
+ * To run a query within a React component, call `useGetDaOsForExploreQuery` and pass it any options that fit your needs.
+ * When your component renders, `useGetDaOsForExploreQuery` returns an object from Apollo Client that contains loading, error, and data properties
+ * you can use to render your UI.
+ *
+ * @param baseOptions options that will be passed into the query, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options;
+ *
+ * @example
+ * const { data, loading, error } = useGetDaOsForExploreQuery({
+ *   variables: {
+ *      first: // value for 'first'
+ *      skip: // value for 'skip'
+ *      orderBy: // value for 'orderBy'
+ *      filter: // value for 'filter'
+ *   },
+ * });
+ */
+export function useGetDaOsForExploreQuery(baseOptions: Apollo.QueryHookOptions<GetDaOsForExploreQuery, GetDaOsForExploreQueryVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useQuery<GetDaOsForExploreQuery, GetDaOsForExploreQueryVariables>(GetDaOsForExploreDocument, options);
+      }
+export function useGetDaOsForExploreLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<GetDaOsForExploreQuery, GetDaOsForExploreQueryVariables>) {
+          const options = {...defaultOptions, ...baseOptions}
+          return Apollo.useLazyQuery<GetDaOsForExploreQuery, GetDaOsForExploreQueryVariables>(GetDaOsForExploreDocument, options);
+        }
+export type GetDaOsForExploreQueryHookResult = ReturnType<typeof useGetDaOsForExploreQuery>;
+export type GetDaOsForExploreLazyQueryHookResult = ReturnType<typeof useGetDaOsForExploreLazyQuery>;
+export type GetDaOsForExploreQueryResult = Apollo.QueryResult<GetDaOsForExploreQuery, GetDaOsForExploreQueryVariables>;
 export const GetFundSentforReviewerDocument = gql`
     query getFundSentforReviewer($type: FundsTransferType, $to: Bytes) {
   fundsTransfers(
