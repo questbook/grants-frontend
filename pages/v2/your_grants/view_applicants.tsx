345576242dd8fe82a4104d2cadd637ca1361a20d
hadi <hadi.mohammad.saleh1@gmail.com>
2022-09-07 11:01:35 +0300

    fix: fixed user not able to open rubric in accepted panel

--- pages/v2/your_grants/view_applicants.tsx
+++ pages/v2/your_grants/view_applicants.tsx
@@ -945,6 +945,7 @@ function ViewApplicants() {
 								applicantsData={applicantsData}
 								onSendFundsClicked={onSendFundsButtonClicked}
 								onBulkSendFundsClicked={onSendFundsButtonClicked}
+								onSetupApplicantEvaluationClicked={() => setRubricDrawerOpen(true)}
 								grantData={grantData}
 							/>
 						</TabPanel>
