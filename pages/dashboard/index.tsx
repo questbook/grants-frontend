643009cc6851bb7d573c64ef0741997be4ceddc9
Rithik Bhandari <rithikbhandari895@gmail.com>
2022-09-09 23:01:05 +0530

    fix: removed invite modal from dashboard

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -16,7 +16,6 @@ import {
 import NavbarLayout from 'src/layout/navbarLayout'
 import { UNIX_TIMESTAMP_MAX, UNIX_TIMESTAMP_MIN } from 'src/utils/generics'
 import { getSupportedChainIdFromWorkspace } from 'src/utils/validationUtils'
-import InviteModal from 'src/v2/components/InviteModal'
 
 // const Tabledata = [
 // 	{
@@ -74,7 +73,6 @@ import InviteModal from 'src/v2/components/InviteModal'
 function DaoDashboard() {
 	const { workspace, subgraphClients } = useContext(ApiClientsContext)!
 
-	const [isInviteModalOpen, setIsInviteModalOpen] = useState(false)
 	const [chainID, setChainId] = React.useState<SupportedChainId>()
 	const [daoID, setDaoId] = React.useState<string>()
 
@@ -358,12 +356,6 @@ function DaoDashboard() {
 							DAO Stats
 						</Text>
 						<Spacer />
-						<Button
-							onClick={() => setIsInviteModalOpen(true)}
-							leftIcon={<LinkIcon />}
-							variant='primaryV2' >
-							Create invite link
-						</Button>
 						{/* <Menu
 							placement="bottom"
 							// align="right"
@@ -520,9 +512,6 @@ function DaoDashboard() {
 					</Flex>
 				</Container>
 			</Container>
-			<InviteModal
-				isOpen={isInviteModalOpen}
-				onClose={() => setIsInviteModalOpen(false)} />
 		</>
 	)
 }
