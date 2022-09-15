f9d7d6d91af52b250ebf19c8e386a2c3691c6652
Richik Chanda <richikchanda1999@gmail.com>
2022-09-15 14:58:35 +0530

    fix: do not open dashboard for reviewer

--- src/v2/components/Sidebar/index.tsx
+++ src/v2/components/Sidebar/index.tsx
@@ -92,8 +92,22 @@ function Sidebar() {
 						workspaces={workspaces}
 						onWorkspaceClick={
 							(index, onComplete: () => void) => {
+								const members = workspaces[index]?.members
+								logger.info({ members }, 'Workspace members')
+								const member = workspaces[index].members.find((member) => {
+									const actorId = member?.actorId
+									const address = accountData?.address
+									logger.info({ actorId, address, isEqual: actorId === address.toLowerCase() }, 'Comparing actorId and address')
+									return member.actorId === accountData?.address?.toLowerCase()
+								})
+								logger.info({ member }, 'Member')
 								setWorkspace(workspaces[index])
-								router.push('/dashboard').then(onComplete)
+								if(member?.accessLevel === 'reviewer') {
+									router.push('/your_grants').then(onComplete)
+								} else {
+									router.push('/dashboard').then(onComplete)
+								}
+
 								const workspaceChainID = getSupportedChainIdFromWorkspace(workspaces[index])
 								if(network !== workspaceChainID) {
 									logger.info('SWITCH NETWORK (sidebar.tsx 1): ', workspaceChainID)
