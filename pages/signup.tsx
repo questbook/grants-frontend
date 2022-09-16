acd9d8c464e56ba3d891924dc4bc0ccd7b4b765e
Richik Chanda <richikchanda1999@gmail.com>
2022-09-16 13:16:35 +0530

    hotfix: first grant creation flow

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -13,6 +13,7 @@ import { SupportedNetwork } from 'src/generated/graphql'
 import useCreateGrant from 'src/hooks/useCreateGrant'
 import useCreateWorkspace from 'src/hooks/useCreateWorkspace'
 import NavbarLayout from 'src/layout/navbarLayout'
+import { getSupportedChainIdFromWorkspace } from 'src/utils/validationUtils'
 import NetworkTransactionModal from 'src/v2/components/NetworkTransactionModal'
 
 function SignupDao() {
@@ -86,7 +87,7 @@ function SignupDao() {
 		grantTransactionData,
 		transactionLink,
 		createGrantLoading,
-	] = useCreateGrant(grantData, setCurrentStep, workspaceData?.network, daoData?.id)
+	] = useCreateGrant(grantData, setCurrentStep, getSupportedChainIdFromWorkspace(workspace), workspace?.id)
 
 	useEffect(() => {
 		// // console.log(grantTransactionData);
