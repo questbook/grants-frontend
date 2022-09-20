5d04bd124192ed040b00c2bfafc78ee8ed2662c8
Richik Chanda <richikchanda1999@gmail.com>
2022-09-20 11:05:37 +0530

    fix: change the order of tabs in View Applicant page

--- pages/v2/your_grants/view_applicants.tsx
+++ pages/v2/your_grants/view_applicants.tsx
@@ -768,13 +768,25 @@ function ViewApplicants() {
 					h={8}
 					colorScheme='brandv2'>
 					<TabList>
-						<StyledTab label={`Accepted (${applicantsData.filter((item: any) => (2 === item.status)).length})`} />
 						<StyledTab label={`In Review (${applicantsData.filter((item: any) => (0 === item.status)).length})`} />
+						<StyledTab label={`Accepted (${applicantsData.filter((item: any) => (2 === item.status)).length})`} />
 						<StyledTab label={`Rejected (${applicantsData.filter((item: any) => (3 === item.status)).length})`} />
 						<StyledTab label={`Asked to Resubmit (${applicantsData.filter((item: any) => (1 === item.status)).length})`} />
 					</TabList>
 
 					<TabPanels>
+						<TabPanel
+							tabIndex={1}
+							borderRadius='2px'
+							p={0}
+							mt={5}
+							bg='white'
+							boxShadow='inset 1px 1px 0px #F0F0F7, inset -1px -1px 0px #F0F0F7'>
+							<InReviewPanel
+								applicantsData={applicantsData}
+								grantData={grantData} />
+						</TabPanel>
+
 						<TabPanel
 							borderRadius='2px'
 							p={0}
@@ -792,18 +804,6 @@ function ViewApplicants() {
 								grantData={grantData}
 								rewardAssetDecimals={rewardAssetDecimals}
 							/>
-						</TabPanel>
-
-						<TabPanel
-							tabIndex={1}
-							borderRadius='2px'
-							p={0}
-							mt={5}
-							bg='white'
-							boxShadow='inset 1px 1px 0px #F0F0F7, inset -1px -1px 0px #F0F0F7'>
-							<InReviewPanel
-								applicantsData={applicantsData}
-								grantData={grantData} />
 						</TabPanel>
 
 						<TabPanel
