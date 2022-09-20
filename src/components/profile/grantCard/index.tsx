8db62ed61a3f1854af8b42ca95c0ec35603c0e24
Sweta Shaw <swt.shw94@gmail.com>
2022-09-20 15:15:09 +0530

    fix reward amount

--- src/components/profile/grantCard/index.tsx
+++ src/components/profile/grantCard/index.tsx
@@ -56,7 +56,7 @@ function BrowseGrantCard({
 	useEffect(() => {
 		if(grantReward === 0 && grantCurrency === 'USD') {
 			setGrantReward(parseInt(grantAmount))
-		}else{
+		}else if(grantCurrency!== 'USD'){
 			calculateUSDValue(grantAmount, grantCurrencyPair).then((promise: any) => {
 				setGrantReward(promise as number)
 			})
@@ -143,7 +143,7 @@ function BrowseGrantCard({
 								color='#373737'
 							>
 								<b>
-									{(grantReward !== 0 && grantCurrency !== 'USD') ? `$${nFormatter(grantReward.toFixed(0))}` : `$${grantAmount}`}
+									{grantReward !== 0 && `$${nFormatter(grantReward.toFixed(0))}`}
 								</b>
 								{' '}
 								{t('/profile.cards.per_proposal')}
