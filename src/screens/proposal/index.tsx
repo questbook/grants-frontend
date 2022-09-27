df4c2bf9a6861f33446e59d9c9b265bb14d792ae
Sweta Shaw <swt.shw94@gmail.com>
2022-09-27 15:26:35 +0530

    structure base code layout

--- src/screens/proposal/index.tsx
+++ src/screens/proposal/index.tsx
@@ -9,71 +9,76 @@ import NavbarLayout from 'src/libraries/ui/navbarLayout'
 import { useMultiChainQuery } from 'src/screens/proposal/_hooks/useMultiChainQuery'
 
 function Proposal() {
-	const router = useRouter()
 
-	const [proposalId, setProposalId] = useState<string>()
-	const [chainId, setChainId] = useState<SupportedChainId>()
+    const buildComponent = () => (
 
-	useEffect(() => {
-		if(typeof router.query.id === 'string') {
-			setProposalId(router.query.id)
-		}
+        <Flex
+            bg='gray.1'
+            w='100vw'
+            h='calc(100vh - 64px)'
+            mx={8}
+            pt={6}>
+            <Flex
+                flex={2}
+                w='100%'
+                h='100%'
+                bg='red' />
+            <Box mx={4} />
+            <Flex
+                flex={1}
+                w='100%'
+                h='100%'
+                bg='blue' />
+        </Flex>
 
-		if(typeof router.query.chain === 'string' && router.query.chain in SupportedChainId) {
-			setChainId(parseInt(router.query.chain) as SupportedChainId)
-		}
-	}, [])
+    )
 
-	const { results, fetchMore } = useMultiChainQuery({
-		useQuery: useGetApplicationDetailsQuery,
-		options: {
-			variables: {
-				applicationID: proposalId ?? '',
-			}
-		},
-		chains: [chainId ?? defaultChainId]
-	})
+    const router = useRouter()
 
-	useEffect(() => {
-		fetchMore({ applicationID: proposalId }, true)
-	}, [proposalId, chainId])
+    const [proposalId, setProposalId] = useState<string>()
+    const [chainId, setChainId] = useState<SupportedChainId>()
 
-	const proposalData = useMemo(() => {
-		return results[0]?.grantApplication
-	}, [results])
+    useEffect(() => {
+        if (typeof router.query.id === 'string') {
+            setProposalId(router.query.id)
+        }
 
-	useEffect(() => {
-		logger.info({ proposalData }, 'Proposal Data')
-	}, [proposalData])
+        if (typeof router.query.chain === 'string' && router.query.chain in SupportedChainId) {
+            setChainId(parseInt(router.query.chain) as SupportedChainId)
+        }
+    }, [])
 
-	return (
-		<Flex
-			bg='gray.1'
-			w='100vw'
-			h='calc(100vh - 64px)'
-			mx={8}
-			pt={6}>
-			<Flex
-				flex={2}
-				w='100%'
-				h='100%'
-				bg='red' />
-			<Box mx={4} />
-			<Flex
-				flex={1}
-				w='100%'
-				h='100%'
-				bg='blue' />
-		</Flex>
-	)
+    const { results, fetchMore } = useMultiChainQuery({
+        useQuery: useGetApplicationDetailsQuery,
+        options: {
+            variables: {
+                applicationID: proposalId ?? '',
+            }
+        },
+        chains: [chainId ?? defaultChainId]
+    })
+
+    useEffect(() => {
+        fetchMore({ applicationID: proposalId }, true)
+    }, [proposalId, chainId])
+
+    const proposalData = useMemo(() => {
+        return results[0]?.grantApplication
+    }, [results])
+
+    useEffect(() => {
+        logger.info({ proposalData }, 'Proposal Data')
+    }, [proposalData])
+
+    return buildComponent()
 }
 
-Proposal.getLayout = function(page: ReactElement) {
-	return (
-		<NavbarLayout>
-			{page}
-		</NavbarLayout>
-	)
+Proposal.getLayout = function (page: ReactElement) {
+    return (
+        <NavbarLayout>
+            {page}
+        </NavbarLayout>
+    )
 }
 
 export default Proposal
