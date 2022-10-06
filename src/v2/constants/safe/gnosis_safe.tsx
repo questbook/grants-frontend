bdfdfc11a2a58176ce156b7ba10fe0e5c4f602bd
Sweta Shaw <swt.shw94@gmail.com>
2022-10-06 15:03:36 +0530

    Integerate Telos

--- src/v2/constants/safe/gnosis_safe.tsx
+++ src/v2/constants/safe/gnosis_safe.tsx
@@ -15,13 +15,13 @@ export class GnosisSafe implements _GnosisSafe {
 	txnServiceURL: string
 
 	constructor(chainId: number, txnServiceURL: string, safeAddress: string) {
-    	this.id = safeAddress
-    	this.name = 'Gnosis Safe'
-    	this.description = 'Gnosis Safe'
-    	this.image = ''
-    	this.chainId = chainId
-    	this.txnServiceURL = txnServiceURL
-	}	
+		this.id = safeAddress
+		this.name = 'Gnosis Safe'
+		this.description = 'Gnosis Safe'
+		this.image = ''
+		this.chainId = chainId
+		this.txnServiceURL = txnServiceURL
+	}
 	isValidRecipientAddress(address: String): Promise<boolean> {
 		return new Promise((resolve, reject) => resolve(ethers.utils.isAddress(address.toString())))
 	}
@@ -33,109 +33,108 @@ export class GnosisSafe implements _GnosisSafe {
 	}
 
 	proposeTransactions(grantName: string, transactions: TransactionType[], wallet: any): Promise<string> {
-    	throw new Error('Method not implemented.')
+		throw new Error('Method not implemented.')
 	}
 
 	async createMultiTransaction(transactions: MetaTransaction[], safeAddress: string) {
 
-    	// console.log('creating gnosis transaction for', transactions)
-    	//@ts-ignore
-    	const provider = new ethers.providers.Web3Provider(window.ethereum)
-    	await provider.send('eth_requestAccounts', [])
+		console.log('creating gnosis transaction for', transactions)
+		//@ts-ignore
+		const provider = new ethers.providers.Web3Provider(window.ethereum)
+		await provider.send('eth_requestAccounts', [])
 
-    	const signer = provider.getSigner()
-    	const ethAdapter = new EthersAdapter({
-    		ethers,
-    		signer,
-    	})
+		const signer = provider.getSigner()
+		const ethAdapter = new EthersAdapter({
+			ethers,
+			signer,
+		})
+		const safeService = new SafeServiceClient({ txServiceUrl: this.txnServiceURL, ethAdapter })
+		// const safeFactory = await SafeFactory.create({ ethAdapter })
+		const safeSdk = await Safe.create({ ethAdapter, safeAddress })
 
-    	const safeService = new SafeServiceClient({ txServiceUrl: this.txnServiceURL, ethAdapter })
-    	// const safeFactory = await SafeFactory.create({ ethAdapter })
-    	const safeSdk = await Safe.create({ ethAdapter, safeAddress })
+		try {
+			const safeTransaction = await safeSdk.createTransaction({ safeTransactionData: transactions })
 
-    	try {
-    		const safeTransaction = await safeSdk.createTransaction(transactions)
+			const safeTxHash = await safeSdk.getTransactionHash(safeTransaction)
+			const senderSignature = await safeSdk.signTransactionHash(safeTxHash)
+			// console.log(await signer.getAddress())
 
-    		const safeTxHash = await safeSdk.getTransactionHash(safeTransaction)
-    		const senderSignature = await safeSdk.signTransactionHash(safeTxHash)
-    		// console.log(await signer.getAddress())
-			
 			// console.log('safe address', safeAddress, safeTransaction.data, safeTxHash, senderSignature.data)
 
-    		await safeService.proposeTransaction({
-    			safeAddress,
-    			safeTransactionData: safeTransaction.data,
-    			safeTxHash,
-    			senderAddress: senderSignature.signer,
-    			senderSignature: senderSignature.data
-    		})
+			await safeService.proposeTransaction({
+				safeAddress,
+				safeTransactionData: safeTransaction.data,
+				safeTxHash,
+				senderAddress: senderSignature.signer,
+				senderSignature: senderSignature.data
+			})
 
-    		return safeTxHash
-    	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-} catch(e: any) {
-    		// return undefined
-    		console.log(e)
-    	}
+			return safeTxHash
+			// eslint-disable-next-line @typescript-eslint/no-explicit-any
+		} catch (e: any) {
+			// return undefined
+			console.log(e)
+		}
 
 
 	}
 
 	async isValidSafeAddress(address: String) {
-    	return false
+		return false
 	}
 
 
 	async isOwner(safeAddress: string): Promise<boolean> {
-    	//@ts-ignore
-    	const provider = new ethers.providers.Web3Provider(window.ethereum)
-    	await provider.send('eth_requestAccounts', [])
+		//@ts-ignore
+		const provider = new ethers.providers.Web3Provider(window.ethereum)
+		await provider.send('eth_requestAccounts', [])
 
-    	const signer = provider.getSigner()
-    	const ethAdapter = new EthersAdapter({
-    		ethers,
-    		signer,
-    	})
+		const signer = provider.getSigner()
+		const ethAdapter = new EthersAdapter({
+			ethers,
+			signer,
+		})
 
-    	const safeSdk = await Safe.create({ ethAdapter, safeAddress })
+		const safeSdk = await Safe.create({ ethAdapter, safeAddress })
 
-    	const userAddress = await signer.getAddress()
-      return await safeSdk.isOwner(userAddress)
+		const userAddress = await signer.getAddress()
+		return await safeSdk.isOwner(userAddress)
 	}
 
 	async getTransactionHashStatus(safeTxHash: string): Promise<any> {
 
-        const safeAddress = this.id
-        // const safeTxnHash = "0x6b93a22e3929062eadf085a6a150d6bf59d0690ff93b0921cbe1c313708be83c"
-        //@ts-ignore
-        const provider = new ethers.providers.Web3Provider(window.ethereum)
-        await provider.send('eth_requestAccounts', [])
+		const safeAddress = this.id
+		// const safeTxnHash = "0x6b93a22e3929062eadf085a6a150d6bf59d0690ff93b0921cbe1c313708be83c"
+		//@ts-ignore
+		const provider = new ethers.providers.Web3Provider(window.ethereum)
+		await provider.send('eth_requestAccounts', [])
 
-        const signer = provider.getSigner()
-        const ethAdapter = new EthersAdapter({
-            ethers,
-            signer,
-        })
-        const safeService = new SafeServiceClient({ txServiceUrl: this.txnServiceURL, ethAdapter })
-        const txnDetails = await safeService.getTransaction(safeTxHash)
-        if(txnDetails.isExecuted) {
-			return {...txnDetails, status: 1}
+		const signer = provider.getSigner()
+		const ethAdapter = new EthersAdapter({
+			ethers,
+			signer,
+		})
+		const safeService = new SafeServiceClient({ txServiceUrl: this.txnServiceURL, ethAdapter })
+		const txnDetails = await safeService.getTransaction(safeTxHash)
+		if (txnDetails.isExecuted) {
+			return { ...txnDetails, status: 1 }
 		} else {
 			return null
 		}
-    }
+	}
 
 
 	async getSafeDetails(address: String): Promise<any> {
 		//@ts-ignore
-        const provider = new ethers.providers.Web3Provider(window.ethereum)
-        await provider.send('eth_requestAccounts', [])
+		const provider = new ethers.providers.Web3Provider(window.ethereum)
+		await provider.send('eth_requestAccounts', [])
 
-        const signer = provider.getSigner()
-        const ethAdapter = new EthersAdapter({
-            ethers,
-            signer,
-        })
-        const safeService = new SafeServiceClient({ txServiceUrl: this.txnServiceURL, ethAdapter })
+		const signer = provider.getSigner()
+		const ethAdapter = new EthersAdapter({
+			ethers,
+			signer,
+		})
+		const safeService = new SafeServiceClient({ txServiceUrl: this.txnServiceURL, ethAdapter })
 		const balanceInUsd = await safeService.getUsdBalances(this.id)
 		return balanceInUsd
 	}
