6301a4e0d9099f9fa7045f828e4a729428c7cb72
Rithik Bhandari <rithikbhandari895@gmail.com>
2022-10-09 03:36:11 +0530

    feat: added useUpdateDaoVisibility hook

---
+++ src/hooks/useUpdateDaoVisibility.ts
@@ -0,0 +1,246 @@
+import React, { useContext, useEffect, useState } from 'react'
+import { ToastId, useToast } from '@chakra-ui/react'
+import ErrorToast from 'src/components/ui/toasts/errorToast'
+import { WORKSPACE_REGISTRY_ADDRESS } from 'src/constants/addresses';
+import useQBContract from 'src/hooks/contracts/useQBContract'
+import { useBiconomy } from 'src/hooks/gasless/useBiconomy'
+import { useNetwork } from 'src/hooks/gasless/useNetwork'
+import { useQuestbookAccount } from 'src/hooks/gasless/useQuestbookAccount'
+import useChainId from 'src/hooks/utils/useChainId'
+import { ApiClientsContext, WebwalletContext } from 'src/pages/_app'
+import getErrorMessage from 'src/utils/errorUtils'
+import { getExplorerUrlForTxHash } from 'src/utils/formattingUtils'
+import { bicoDapps, chargeGas, getTransactionDetails, sendGaslessTransaction } from 'src/utils/gaslessUtils'
+import logger from 'src/utils/logger'
+import {
+  getSupportedChainIdFromWorkspace,
+} from 'src/utils/validationUtils'
+
+export default function useUpdateDaoVisibility(
+  data: { [_: string]: boolean },
+  setCurrentStep: (step: number | undefined) => void
+) {
+  const keys = Object.keys(data)
+
+  const workspaceIds: string[] = [];
+  const isVisible: boolean[] = [];
+
+  keys.forEach((key) => {
+    workspaceIds.push(key)
+    isVisible.push(data[key])
+  })
+
+  const [error, setError] = useState<string>()
+  const [loading, setLoading] = useState(false)
+  const [incorrectNetwork, setIncorrectNetwork] = useState(false)
+  const [transactionData, setTransactionData] = useState<any>()
+  const { data: accountData, nonce } = useQuestbookAccount()
+  const { data: networkData, switchNetwork } = useNetwork()
+
+  const apiClients = useContext(ApiClientsContext)!
+  const { validatorApi, workspace, subgraphClients } = apiClients
+  const currentChainId = useChainId()
+  const chainId = getSupportedChainIdFromWorkspace(workspace)
+  const workspaceContract = useQBContract('workspace', chainId)
+  const toastRef = React.useRef<ToastId>()
+  const toast = useToast()
+
+  const { webwallet } = useContext(WebwalletContext)!
+
+  const { biconomyDaoObj: biconomy, biconomyWalletClient, scwAddress, loading: biconomyLoading } = useBiconomy({
+    chainId: chainId?.toString()!
+  })
+
+  const [isBiconomyInitialised, setIsBiconomyInitialised] = React.useState(false)
+
+  useEffect(() => {
+    if(biconomy && biconomyWalletClient && scwAddress && !biconomyLoading && chainId && biconomy.networkId &&
+      biconomy.networkId.toString() === chainId.toString()) {
+      setIsBiconomyInitialised(true)
+    }
+  }, [biconomy, biconomyWalletClient, scwAddress, biconomyLoading, isBiconomyInitialised])
+
+  useEffect(() => {
+    if(incorrectNetwork) {
+      setIncorrectNetwork(false)
+    }
+
+  }, [workspaceContract])
+
+  useEffect(() => {
+    if(incorrectNetwork) {
+      return
+    }
+
+    if(error) {
+      return
+    }
+
+    if(loading) {
+      return
+    }
+
+    async function validate() {
+      setCurrentStep(1)
+      setLoading(true)
+      try {
+
+        if(!biconomyWalletClient || typeof biconomyWalletClient === 'string' || !scwAddress) {
+          throw new Error('Zero wallet is not ready')
+        }
+
+        // TODO
+        // const {
+        //   data: { ipfsHash },
+        // } = await validatorApi.({
+        // })
+        // if(!ipfsHash) {
+        //   throw new Error('Error validating grant data')
+        // }
+
+        const response = await sendGaslessTransaction(
+          biconomy,
+          workspaceContract,
+          'updateWorkspacesVisible',
+          [
+            workspaceIds,
+            isVisible,
+          ],
+          WORKSPACE_REGISTRY_ADDRESS[currentChainId],
+          biconomyWalletClient,
+          scwAddress,
+          webwallet,
+          `${currentChainId}`,
+          bicoDapps[currentChainId].webHookId,
+          nonce
+        )
+
+        if(!response) {
+          return
+        }
+
+        setCurrentStep(2)
+
+        if(response) {
+          const { receipt, txFee } = await getTransactionDetails(response, currentChainId.toString())
+          await subgraphClients[currentChainId].waitForBlock(receipt?.blockNumber)
+          setCurrentStep(3)
+
+          setTransactionData(receipt)
+          await chargeGas(Number(workspace?.id), Number(txFee))
+          setCurrentStep(4)
+        }
+
+        setLoading(false)
+        setCurrentStep(5)
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      } catch(e) {
+        const message = getErrorMessage(e as Error)
+        setError(message)
+        setLoading(false)
+        toastRef.current = toast({
+          position: 'top',
+          render: () => ErrorToast({
+            content: message,
+            close: () => {
+              if(toastRef.current) {
+                toast.close(toastRef.current)
+              }
+            },
+          }),
+        })
+      }
+    }
+
+    try {
+      if(!data || !Object.keys(data).length) {
+        return
+      }
+
+      if(transactionData) {
+        return
+      }
+
+      if(!accountData || !accountData.address) {
+        throw new Error('not connected to wallet')
+      }
+
+      if(!workspace) {
+        throw new Error('not connected to workspace')
+      }
+
+      if(!currentChainId) {
+        if(switchNetwork && chainId) {
+          logger.info('SWITCH NETWORK (use-update-application-state.tsx 1): ', chainId)
+          switchNetwork(chainId)
+        }
+
+        setIncorrectNetwork(true)
+        setLoading(false)
+        return
+      }
+
+      if(chainId !== currentChainId) {
+        if(switchNetwork && chainId) {
+          logger.info('SWITCH NETWORK (use-update-application-state.tsx 2): ', chainId)
+          switchNetwork(chainId)
+        }
+
+        setIncorrectNetwork(true)
+        setLoading(false)
+        return
+      }
+
+      if(!validatorApi) {
+        throw new Error('validatorApi or workspaceId is not defined')
+      }
+
+      // if(
+      //   !applicationContract
+      //   || applicationContract.address
+      //   === '0x0000000000000000000000000000000000000000'
+      // ) {
+      //   return
+      // }
+
+      validate()
+    } catch(e) {
+      const message = getErrorMessage(e as Error)
+      setError(message)
+      setLoading(false)
+      toastRef.current = toast({
+        position: 'top',
+        render: () => ErrorToast({
+          content: message,
+          close: () => {
+            if(toastRef.current) {
+              toast.close(toastRef.current)
+            }
+          },
+        }),
+      })
+    }
+
+  }, [
+    error,
+    loading,
+    toast,
+    transactionData,
+    workspaceContract,
+    validatorApi,
+    workspace,
+    accountData,
+    networkData,
+    currentChainId,
+    data,
+    incorrectNetwork,
+    chainId,
+  ])
+
+  return [
+    transactionData,
+    getExplorerUrlForTxHash(currentChainId, transactionData?.transactionHash),
+    loading,
+    isBiconomyInitialised,
+  ]
+}
