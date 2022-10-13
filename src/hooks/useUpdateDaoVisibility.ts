68971c2261562f4df279bec09c418fc5b87f9514
Rithik Bhandari <rithikbhandari895@gmail.com>
2022-10-14 01:42:26 +0530

    fix: fix workspaceContract for different chains

--- src/hooks/useUpdateDaoVisibility.ts
+++ src/hooks/useUpdateDaoVisibility.ts
@@ -1,36 +1,36 @@
 import { useCallback, useContext, useEffect, useState } from 'react'
+import { Contract } from 'ethers'
 import { WORKSPACE_REGISTRY_ADDRESS } from 'src/constants/addresses'
-import { SupportedChainId } from 'src/constants/chains'
+import { defaultChainId, SupportedChainId } from 'src/constants/chains'
 import useQBContract from 'src/hooks/contracts/useQBContract'
 import { useBiconomy } from 'src/hooks/gasless/useBiconomy'
 import { useQuestbookAccount } from 'src/hooks/gasless/useQuestbookAccount'
 import { ApiClientsContext, BiconomyContext, WebwalletContext } from 'src/pages/_app'
 import { bicoDapps, chargeGas, getTransactionDetails, sendGaslessTransaction } from 'src/utils/gaslessUtils'
-import {
-	getSupportedChainIdFromWorkspace,
-} from 'src/utils/validationUtils'
 
 export default function useUpdateDaoVisibility() {
 	const { nonce } = useQuestbookAccount()
 
 	const { workspace, subgraphClients } = useContext(ApiClientsContext)!
 
-	const chainId = getSupportedChainIdFromWorkspace(workspace)
-	const workspaceContract = useQBContract('workspace', chainId)
+	const workspaceContractGoerli = useQBContract('workspace', SupportedChainId.GOERLI_TESTNET)
+	const workspaceContractPolygon = useQBContract('workspace', SupportedChainId.POLYGON_MAINNET)
+	const workspaceContractOptimism = useQBContract('workspace', SupportedChainId.OPTIMISM_MAINNET)
+	const workspaceContractCelo = useQBContract('workspace', SupportedChainId.CELO_MAINNET)
 
-	const { webwallet } = useContext(WebwalletContext)!
+	const { webwallet, scwAddress } = useContext(WebwalletContext)!
 
 	const { initiateBiconomy } = useContext(BiconomyContext)!
 
-	const { biconomyDaoObj: biconomy, biconomyWalletClient, scwAddress, loading: biconomyLoading } = useBiconomy({
-		chainId: chainId?.toString()!
+	const { biconomyDaoObj: biconomy, biconomyWalletClient, loading: biconomyLoading } = useBiconomy({
+		chainId: defaultChainId?.toString()!
 	})
 
 	const [isBiconomyInitialised, setIsBiconomyInitialised] = useState(false)
 
 	useEffect(() => {
-		if(biconomy && biconomyWalletClient && scwAddress && !biconomyLoading && chainId && biconomy.networkId &&
-      biconomy.networkId.toString() === chainId.toString()) {
+		if(biconomy && biconomyWalletClient && scwAddress && !biconomyLoading && biconomy.networkId &&
+      biconomy.networkId.toString() === defaultChainId.toString()) {
 			setIsBiconomyInitialised(true)
 		}
 	}, [biconomy, biconomyWalletClient, scwAddress, biconomyLoading, isBiconomyInitialised])
@@ -41,7 +41,7 @@ export default function useUpdateDaoVisibility() {
 			stepsLength: number,
 			setCurrentStep?: (step: number | undefined) => void
 		) => {
-			if(typeof biconomyWalletClient === 'string' || !biconomyWalletClient || !scwAddress || !chainId) {
+			if(typeof biconomyWalletClient === 'string' || !biconomyWalletClient || !scwAddress) {
 				return undefined!
 			}
 
@@ -51,6 +51,26 @@ export default function useUpdateDaoVisibility() {
 			for(chainIdx = 0; chainIdx < chains.length; chainIdx ++) {
 				const chain = chains[chainIdx]
 				const chainId = +chain
+
+				let workspaceContract: Contract
+
+				switch (chainId) {
+				case 5:
+					workspaceContract = workspaceContractGoerli
+					break
+				case 10:
+					workspaceContract = workspaceContractOptimism
+					break
+				case 137:
+					workspaceContract = workspaceContractPolygon
+					break
+				case 42220:
+					workspaceContract = workspaceContractCelo
+					break
+				default:
+					continue
+				}
+
 
 				setCurrentStep?.(chainIdx * stepsLength)
 				await initiateBiconomy(chains[chainIdx])
@@ -70,11 +90,13 @@ export default function useUpdateDaoVisibility() {
 				setCurrentStep?.(chainIdx * stepsLength + 1)
 
 				const response = await sendGaslessTransaction(
+					// FIXME
 					biconomy,
 					workspaceContract,
 					'updateWorkspacesVisible',
 					[workspaceIds, isVisible],
 					WORKSPACE_REGISTRY_ADDRESS[chains[chainIdx] as unknown as SupportedChainId],
+					// FIXME
 					biconomyWalletClient,
 					scwAddress,
 					webwallet,
@@ -98,7 +120,7 @@ export default function useUpdateDaoVisibility() {
 
 			setCurrentStep?.(chainIdx * stepsLength + 1)
 		},
-		[workspace?.id, workspaceContract, biconomyWalletClient, chainId, scwAddress, biconomy, nonce, webwallet]
+		[workspace?.id, workspaceContractGoerli, workspaceContractPolygon, workspaceContractCelo, workspaceContractOptimism, scwAddress, nonce, webwallet]
 	)
 
 	return {
