74ee129316081b544200486723a22ac91838197f
Richik Chanda <richikchanda1999@gmail.com>
2022-12-19 13:50:53 +0530

    fix: broken zero state for Reviews

--- src/generated/contracts/factories/ApplicationReviewRegistryAbi__factory.ts
+++ src/generated/contracts/factories/ApplicationReviewRegistryAbi__factory.ts
@@ -1069,7 +1069,7 @@ const _abi = [
     stateMutability: "view",
     type: "function",
   },
-] as const;
+];
 
 export class ApplicationReviewRegistryAbi__factory {
   static readonly abi = _abi;
