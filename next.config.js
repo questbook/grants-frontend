a71a85efb9d9e977df41cf4980b6590d76499b26
Richik Chanda <richikchanda1999@gmail.com>
2023-01-20 00:39:18 +0530

    chore: updated next.config.js to export multiple modules

--- next.config.js
+++ next.config.js
@@ -37,6 +37,12 @@ const nextConfig = {
 	},
 }
 
-module.exports = withSentryConfig(nextConfig)
+const withPWA = require('next-pwa')({
+	dest: 'public',
+	disable: process.env.NODE_ENV === 'development',
+	register: true,
+})
 
-module.exports = withYaml(nextConfig)
+module.exports = withSentryConfig(withYaml(withPWA(nextConfig)))
+
+
