53dbb8f8a71e3e3edb6e582b8808fa7933f1eadd
Mohammad Shahin <59062739+MohammadShahin@users.noreply.github.com>
2023-03-14 14:28:29 +0300

    fix: lint & prettier

--- src/libraries/ui/NavBar/_components/BackupWallet.tsx
+++ src/libraries/ui/NavBar/_components/BackupWallet.tsx
@@ -4,11 +4,11 @@ import copy from 'copy-to-clipboard'
 import { ethers, Wallet } from 'ethers'
 import useCustomToast from 'src/libraries/hooks/useCustomToast'
 interface Props {
-    privateKey: string
-    inited: boolean
-    loading: boolean
-    exportWalletToGD: (wallet: Wallet) => Promise<void>
-    isNewUser: boolean
+	privateKey: string
+	inited: boolean
+	loading: boolean
+	exportWalletToGD: (wallet: Wallet) => Promise<void>
+	isNewUser: boolean
 }
 export default function BackupWallet({ isNewUser, exportWalletToGD, loading, inited, privateKey }: Props) {
 	const toast = useCustomToast()
@@ -17,66 +17,83 @@ export default function BackupWallet({ isNewUser, exportWalletToGD, loading, ini
 	}, [loading, inited])
 	return (
 		<>
-		<Flex
-		flexDirection='column'
-		paddingRight='19px'
-		paddingLeft='19px'
-		>
-			<Text
-				variant='v2_subheading'
-				fontWeight='500'
-				alignSelf='center'
-				fontSize={14}>
-				{title}
-			</Text>
-			<Text
-				variant='v2_body'
-				align='center'
-
-				fontSize={13}
-				color='black.3'>
-				You will need the private key to sign into Questbook again. Save it in a
-				secure place.
-			</Text>
-			<Textarea
-				readOnly
-				fontSize={[13, 15]}
-				w='100%'
-				mt={12}
-				value={privateKey}
-			/>
 			<Flex
-				flexDirection='row'
-				gap={3}
-				width='100%'
-				marginTop={2}
-				marginBottom={5}
+				flexDirection='column'
+				paddingRight='19px'
+				paddingLeft='19px'
 			>
-				<Button
-					width='50%'
-					bg='gray.3'
-					height={10}
-					w='90%'
-					borderRadius='20'
-					// variant='primaryMedium'
-					marginTop={4}
-					isDisabled={loading || !inited}
-					onClick={
-						async() => {
-							try {
-								await exportWalletToGD(new ethers.Wallet(privateKey))
-								toast({
-									title: 'Saved in Google Drive',
-									status: 'success',
-									duration: 3000,
-									isClosable: true,
-								})
-							} catch(error) {
-								// this means the error is generated from google itself
-								if(error && typeof error === 'object' && 'type' in error) {
-									if(error.type === 'popup_closed') {
+				<Text
+					variant='v2_subheading'
+					fontWeight='500'
+					alignSelf='center'
+					fontSize={14}>
+					{title}
+				</Text>
+				<Text
+					variant='v2_body'
+					align='center'
+
+					fontSize={13}
+					color='black.3'>
+					You will need the private key to sign into Questbook again. Save it in a
+					secure place.
+				</Text>
+				<Textarea
+					readOnly
+					fontSize={[13, 15]}
+					w='100%'
+					mt={12}
+					value={privateKey}
+				/>
+				<Flex
+					flexDirection='row'
+					gap={3}
+					width='100%'
+					marginTop={2}
+					marginBottom={5}
+				>
+					<Button
+						width='50%'
+						bg='gray.3'
+						height={10}
+						w='90%'
+						borderRadius='20'
+						// variant='primaryMedium'
+						marginTop={4}
+						isDisabled={loading || !inited}
+						onClick={
+							async() => {
+								try {
+									await exportWalletToGD(new ethers.Wallet(privateKey))
+									toast({
+										title: 'Saved in Google Drive',
+										status: 'success',
+										duration: 3000,
+										isClosable: true,
+									})
+								} catch(error) {
+									// this means the error is generated from google itself
+									if(error && typeof error === 'object' && 'type' in error) {
+										if(error.type === 'popup_closed') {
+											toast({
+												title: 'Google popup closed',
+												status: 'error',
+												duration: 3000,
+												isClosable: true,
+											})
+										} else {
+											toast({
+												title: 'Failed to save.',
+												description: 'Unknown error',
+												status: 'error',
+												duration: 3000,
+												isClosable: true,
+											})
+										}
+									} else if(error && typeof error === 'string') {
 										toast({
-											title: 'Google popup closed',
+											title: 'Failed to save.',
+											description: error,
 											status: 'error',
 											duration: 3000,
 											isClosable: true,
@@ -90,70 +107,53 @@ export default function BackupWallet({ isNewUser, exportWalletToGD, loading, ini
 											isClosable: true,
 										})
 									}
-								} else if(error && typeof error === 'string') {
+								}
+							}
+						}
+
+					>
+						<Text
+							variant='v2_body'
+							color='black'
+							fontSize={['11px', '15px']}
+							fontWeight='500'
+						>
+							Import to Google Drive
+						</Text>
+					</Button>
+
+					<Button
+						// variant='primaryMedium'
+						marginTop={4}
+						width='90%'
+						bg='gray.3'
+						height={10}
+						borderRadius='20'
+
+						onClick={
+							() => {
+								const copied = copy(privateKey)
+								if(copied) {
 									toast({
-										title: 'Failed to save.',
-										description: error,
-										status: 'error',
-										duration: 3000,
-										isClosable: true,
-									})
-								} else {
-									toast({
-										title: 'Failed to save.',
-										description: 'Unknown error',
-										status: 'error',
-										duration: 3000,
+										title: 'Copied to clipboard',
+										status: 'success',
+										duration: 2000,
 										isClosable: true,
 									})
 								}
 							}
 						}
-					}
-
-				>
-					<Text
-						variant='v2_body'
-						color='black'
-						fontSize={['11px', '15px']}
-						fontWeight='500'
 					>
-						Import to Google Drive
-					</Text>
-				</Button>
-
-				<Button
-				// variant='primaryMedium'
-					marginTop={4}
-					width='90%'
-					bg='gray.3'
-					height={10}
-					borderRadius='20'
-
-					onClick={
-						() => {
-							const copied = copy(privateKey)
-							if(copied) {
-								toast({
-									title: 'Copied to clipboard',
-									status: 'success',
-									duration: 2000,
-									isClosable: true,
-								})
-							}
-						}
-					}
-				>
-					<Text
-						variant='v2_body'
-						color='black'
-						fontSize={['11px', '15px']}
-						fontWeight='500'
-					>
-						Back up manually
-					</Text>
-				</Button>
-			</Flex>
+						<Text
+							variant='v2_body'
+							color='black'
+							fontSize={['11px', '15px']}
+							fontWeight='500'
+						>
+							Back up manually
+						</Text>
+					</Button>
+				</Flex>
 			</Flex>
 		</>
 	)
