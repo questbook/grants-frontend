3a97534fd2a2c94f32cae0f86f9b7a51c652371a
Ali <ali.salloum.gh@gmail.com>
2023-06-06 15:06:18 +0300

    added the warning banner

---
+++ src/libraries/ui/NavBar/_components/OptimismWarning.tsx
@@ -0,0 +1,37 @@
+import { FaExclamationCircle } from 'react-icons/fa'
+import { Flex, Icon, Link, Text } from '@chakra-ui/react'
+
+const OptimismWarning = () => {
+	return (
+		<Flex
+			align='center'
+			justify='center'
+			bg='#C2E7DA'
+			color='white'
+			p={4}>
+			<Icon
+				as={FaExclamationCircle}
+				boxSize={6}
+				color='#1D1919'
+				mr={2} />
+			<Text
+				// fontWeight='bold'
+				fontSize='14px'
+			>
+				Optimism mainnet is undergoing a network upgrade. As a result, you
+				will not be able to perform any on-chain transactions on Questbook
+				until June 6, 20:00 UTC
+				<Link
+					ml={2}
+					color='#0A84FF'
+					target='_blank'
+					rel='noopener noreferrer'
+					href='https://oplabs.notion.site/Bedrock-Mission-Control-EXTERNAL-fca344b1f799447cb1bcf3aae62157c5'>
+					Learn More
+				</Link>
+			</Text>
+		</Flex>
+	)
+}
+
+export default OptimismWarning
