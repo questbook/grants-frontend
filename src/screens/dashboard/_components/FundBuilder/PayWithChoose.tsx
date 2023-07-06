1f0cdc8b8a8ebc79e3edd7886f14ab26f2490348
hasankhadra <59062695+hasankhadra@users.noreply.github.com>
2023-07-06 19:33:17 +0300

    add: ton coin icon

--- src/screens/dashboard/_components/FundBuilder/PayWithChoose.tsx
+++ src/screens/dashboard/_components/FundBuilder/PayWithChoose.tsx
@@ -68,7 +68,6 @@ function PayWithChoose({ selectedMode }: { selectedMode: {logo: string | undefin
 											setSelectedTokenIndex(value.index)
 										}
 									} />
-							
 							<Text
 									color='#53514F'
 									fontSize='14px'
@@ -105,7 +104,7 @@ function PayWithChoose({ selectedMode }: { selectedMode: {logo: string | undefin
 				<Image
 					boxSize='16px'
 					src={data.tokenIcon}
-					fallbackSrc={data.tokenName.includes('DAI') ? '/chain_assets/dai.svg' : '/chain_assets/eth.svg'}
+					fallbackSrc={(data.tokenName.includes('DAI') ? '/chain_assets/dai.svg' : (data.tokenName.includes('TON') ? '/chain_assets/toncoin.svg' : '/chain_assets/eth.svg'))}
 				/>
 				<Text
 				ml={2}
@@ -132,7 +131,7 @@ function PayWithChoose({ selectedMode }: { selectedMode: {logo: string | undefin
 		>
 				<Image
 				src={data.tokenIcon}
-				fallbackSrc={data.tokenName.includes('DAI') ? '/chain_assets/dai.svg' : '/chain_assets/eth.svg'}
+				fallbackSrc={(data.tokenName.includes('DAI') ? '/chain_assets/dai.svg' : (data.tokenName.includes('TON') ? '/chain_assets/toncoin.svg' : '/chain_assets/eth.svg'))}
 				boxSize='16px' />
 				<Text
 				ml={2}
