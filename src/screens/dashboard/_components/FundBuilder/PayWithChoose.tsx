7ea6a06a7e985bc0cc7d7c061c5807f2b3653182
alimansour0002 <alimansour0002@gmail.com>
2023-07-11 11:42:27 +0300

    fixing bugs

--- src/screens/dashboard/_components/FundBuilder/PayWithChoose.tsx
+++ src/screens/dashboard/_components/FundBuilder/PayWithChoose.tsx
@@ -9,7 +9,7 @@ import DropdownSelect from 'src/libraries/ui/LinkYourMultisigModal/DropdownSelec
 import { FundBuilderContext } from 'src/screens/dashboard/Context'
 
 type DropdownItem = TokenDetailsInterface & { index: number }
-function PayWithChoose({ selectedMode }: { selectedMode: {logo: string | undefined, value: string | undefined} | undefined}) {
+function PayWithChoose({ selectedMode }: { selectedMode: { logo: string | undefined, value: string | undefined } | undefined }) {
 	const buildComponent = () => {
 		return (
 			<Flex
@@ -44,46 +44,49 @@ function PayWithChoose({ selectedMode }: { selectedMode: {logo: string | undefin
 					) : (
 						<>
 							<DropdownSelect
-									options={
-										(tokenList ?? []).map((token: TokenDetailsInterface, index: number) => {
-											const ret = {
-												index,
-												...token
-											}
-											return ret
-										})
-									}
-									placeholder='Select Token'
-									singleValue={singleValue}
-									makeOption={makeOption}
-									selected={{ ...tokenList?.[selectedTokenIndex]!, index: selectedTokenIndex }}
-									setSelected={
-										(value: DropdownItem | undefined) => {
-											logger.info({ value }, 'Clicked')
-											if(!value) {
-												return
-											}
-
-											logger.info({ value }, 'Selected Token')
-											setSelectedTokenIndex(value.index)
+								options={
+									(tokenList ?? []).map((token: TokenDetailsInterface, index: number) => {
+										const ret = {
+											index,
+											...token
 										}
-									} />
-							<Text
+										return ret
+									})
+								}
+								placeholder='Select Token'
+								singleValue={singleValue}
+								makeOption={makeOption}
+								selected={{ ...tokenList?.[selectedTokenIndex]!, index: selectedTokenIndex }}
+								setSelected={
+									(value: DropdownItem | undefined) => {
+										logger.info({ value }, 'Clicked')
+										if (!value) {
+											return
+										}
+
+										logger.info({ value }, 'Selected Token')
+										setSelectedTokenIndex(value.index)
+									}
+								} />
+							{
+								selectedTokenInfo?.tokenName != 'TON' &&
+								<Text
 									color='#53514F'
 									fontSize='14px'
 									mt='8px'>
-								Available:
-								{' '}
-								{parseFloat(selectedTokenInfo?.tokenValueAmount?.toString()!).toFixed(2)}
-								{' '}
-								{selectedTokenInfo?.tokenName}
-								{' '}
-								≈
-								{' '}
-								{parseFloat(selectedTokenInfo?.usdValueAmount?.toString()!).toFixed(2)}
-								{' '}
-								USD
-							</Text>
+									Available:
+									{' '}
+									{parseFloat(selectedTokenInfo?.tokenValueAmount?.toString()!).toFixed(2)}
+									{' '}
+									{selectedTokenInfo?.tokenName}
+									{' '}
+									≈
+									{' '}
+									{parseFloat(selectedTokenInfo?.usdValueAmount?.toString()!).toFixed(2)}
+									{' '}
+									USD
+								</Text>
+							}
 						</>
 					)
 				}
@@ -92,14 +95,14 @@ function PayWithChoose({ selectedMode }: { selectedMode: {logo: string | undefin
 	}
 
 	function makeOption<T extends object>({ innerProps, data }: OptionProps<T, false, GroupBase<T>>) {
-		if(!('tokenName' in data) || !('tokenIcon' in data) || (typeof data.tokenName !== 'string') || (typeof data.tokenIcon !== 'string')) {
+		if (!('tokenName' in data) || !('tokenIcon' in data) || (typeof data.tokenName !== 'string') || (typeof data.tokenIcon !== 'string')) {
 			return <Flex />
 		}
 
 		return (
 			<Flex
-			{...innerProps}
-			w='100%'
+				{...innerProps}
+				w='100%'
 			>
 				<Image
 					boxSize='16px'
@@ -107,9 +110,9 @@ function PayWithChoose({ selectedMode }: { selectedMode: {logo: string | undefin
 					fallbackSrc={(data.tokenName.includes('DAI') ? '/chain_assets/dai.svg' : (data.tokenName.includes('TON') ? '/chain_assets/toncoin.svg' : '/chain_assets/eth.svg'))}
 				/>
 				<Text
-				ml={2}
-				variant='body'
-			>
+					ml={2}
+					variant='body'
+				>
 					{data.tokenName}
 				</Text>
 			</Flex>
@@ -117,26 +120,26 @@ function PayWithChoose({ selectedMode }: { selectedMode: {logo: string | undefin
 	}
 
 	function singleValue<T extends object>({ innerProps, data }: SingleValueProps<T, false, GroupBase<T>>) {
-		if(!('tokenName' in data) || !('tokenIcon' in data) || (typeof data.tokenName !== 'string') || (typeof data.tokenIcon !== 'string')) {
+		if (!('tokenName' in data) || !('tokenIcon' in data) || (typeof data.tokenName !== 'string') || (typeof data.tokenIcon !== 'string')) {
 			return <Flex />
 		}
 
 		return (
 			<Flex
-			{...innerProps}
-			display='inline-flex'
-			alignItems='center'
-			p={0}
-			m={0}
-		>
-				<Image
-				src={data.tokenIcon}
-				fallbackSrc={(data.tokenName.includes('DAI') ? '/chain_assets/dai.svg' : (data.tokenName.includes('TON') ? '/chain_assets/toncoin.svg' : '/chain_assets/eth.svg'))}
-				boxSize='16px' />
-				<Text
-				ml={2}
-				variant='body'
+				{...innerProps}
+				display='inline-flex'
+				alignItems='center'
+				p={0}
+				m={0}
 			>
+				<Image
+					src={data.tokenIcon}
+					fallbackSrc={(data.tokenName.includes('DAI') ? '/chain_assets/dai.svg' : (data.tokenName.includes('TON') ? '/chain_assets/toncoin.svg' : '/chain_assets/eth.svg'))}
+					boxSize='16px' />
+				<Text
+					ml={2}
+					variant='body'
+				>
 					{data.tokenName}
 				</Text>
 			</Flex>
@@ -148,27 +151,27 @@ function PayWithChoose({ selectedMode }: { selectedMode: {logo: string | undefin
 	const [selectedTokenIndex, setSelectedTokenIndex] = useState<number>(selectedTokenInfo ? tokenList?.map((v) => v.tokenName).indexOf(selectedTokenInfo.tokenName)! : 0)
 
 	useEffect(() => {
-		if(!safeObj) {
+		if (!safeObj) {
 			return
 		}
 
-		safeObj?.getTokenAndbalance().then((list: {value?: TokenDetailsInterface[] | undefined, error?: string}) => {
-			if(list?.value) {
-			setTokenList(list?.value)
-			if(list?.value?.length && !selectedTokenInfo) {
-				setSelectedTokenInfo(list?.value[0])
-				setSelectedTokenIndex(0)
+		safeObj?.getTokenAndbalance().then((list: { value?: TokenDetailsInterface[] | undefined, error?: string }) => {
+			if (list?.value) {
+				setTokenList(list?.value)
+				if (list?.value?.length && !selectedTokenInfo) {
+					setSelectedTokenInfo(list?.value[0])
+					setSelectedTokenIndex(0)
+				}
 			}
-		}
 		})
 	}, [safeObj])
 
 	useEffect(() => {
-		if(!tokenList) {
+		if (!tokenList) {
 			return
 		}
 
-		if(selectedTokenIndex < tokenList.length) {
+		if (selectedTokenIndex < tokenList.length) {
 			const token = tokenList[selectedTokenIndex]
 			setSelectedTokenInfo(token)
 		}
