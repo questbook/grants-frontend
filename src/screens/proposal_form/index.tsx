caf93ac6cfa43e8c0153033f42d8cf80652ba392
kryptocodes <srivatsantb@gmail.com>
2023-11-06 22:28:07 +0530

    fix: notification modal
    Signed-off-by: kryptocodes <srivatsantb@gmail.com>

--- src/screens/proposal_form/index.tsx
+++ src/screens/proposal_form/index.tsx
@@ -1,5 +1,5 @@
 import { ChangeEvent, ReactElement, useContext, useEffect, useMemo, useState } from 'react'
-import { Button, Container, Flex, Image, Text, useMediaQuery } from '@chakra-ui/react'
+import { Button, Container, Flex, Image, Text } from '@chakra-ui/react'
 import { convertToRaw } from 'draft-js'
 import { useRouter } from 'next/router'
 import config from 'src/constants/config.json'
@@ -33,7 +33,6 @@ function ProposalForm() {
 	}
 
 	const successComponent = () => {
-		const [isMobile] = useMediaQuery(['(max-width:600px)'])
 		return (
 			<Flex
 				w='100%'
@@ -46,25 +45,23 @@ function ProposalForm() {
 					bg='white'
 					boxShadow='0px 2px 4px rgba(29, 25, 25, 0.1)'
 					overflowY='auto'
-					direction={isMobile ? 'column' : 'row'}
 					my={5}>
 					<Flex
 						direction='column'
 						bg='accent.columbia'
-						w={isMobile ? '100%' : '50%'}
+						w='50%'
 						h='100%'
 						justify='center'
-						align={isMobile ? 'center' : 'start'}
+						align='start'
 						pl='10%'>
 
 						<Image
 							src={grant?.workspace?.logoIpfsHash === config.defaultDAOImageHash ? getAvatar(true, grant?.workspace?.title) : getUrlForIPFSHash(grant?.workspace?.logoIpfsHash!)}
-							boxSize={isMobile ? '10rem' : '20rem' }
+							boxSize='20rem'
 						/>
 						<Text
 							mt={6}
 							variant='heading2'
-							textAlign={isMobile ? 'center' : 'left'}
 							fontWeight='500'>
 							Fantastic — we have received
 							your proposal.
@@ -72,9 +69,8 @@ function ProposalForm() {
 					</Flex>
 					<Flex
 						bg='white'
-						w={isMobile ? '100%' : '50%'}
+						w='50%'
 						h='100%'
-						mt={isMobile ? 6 : 0}
 						direction='column'
 						align='start'
 						justify='center'
