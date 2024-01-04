69b8818467959b17c692038c0295832c5ab66afe
kryptocodes <srivatsantb@gmail.com>
2024-01-04 22:44:44 +0530

    feat: updated queryResult type
    Signed-off-by: kryptocodes <srivatsantb@gmail.com>

--- src/libraries/hooks/useQuery.ts
+++ src/libraries/hooks/useQuery.ts
@@ -7,38 +7,48 @@ type UseQueryOptions = {
     query: DocumentNode
 };
 
-// Define the type for the expected result of the query
-type QueryResult<T> = T | undefined;
 
+type QueryResult<T> = T | undefined;
 export function useQuery<T>({ query }: UseQueryOptions) {
 	const [results, setResults] = useState<QueryResult<T>>(undefined)
 
+	async function fetchMore(variables?: Partial<T>, reset?: boolean, retries = 2): Promise<T | undefined> {
+		if(reset) {
+			setResults(undefined)
+		}
+
+		try {
+			const { data, error } = await client.query<T>({
+				query,
+				variables,
+			})
+
+			if(data) {
+				setResults(data)
+			}
+
+			if(error) {
+				logger.warn(error)
+				if(retries > 0) {
+					await fetchMore(variables, reset, retries - 1)
+					logger.info(`Retrying... Attempts left: ${retries}`)
+				}
+			}
+
+			return data
+		} catch(error) {
+			if(error && retries > 0) {
+				logger.info(`Encountered 503 error. Retrying... Attempts left: ${retries}`)
+				return await fetchMore(variables, reset, retries - 2)
+			} else {
+				logger.warn('Retries exhausted or different error encountered.')
+				return undefined
+			}
+		}
+	}
+
 	return {
 		results,
-		async fetchMore(variables?: Partial<T>, reset?: boolean) {
-			if(reset) {
-				setResults(undefined)
-			}
-
-			try {
-				const { data, error } = await client.query<T>({
-					query,
-					variables,
-				})
-
-				if(data) {
-					setResults(data)
-				}
-
-				if(error) {
-					logger.warn(error)
-				}
-
-				return data // Return the updated data directly
-			} catch(error) {
-				logger.warn(error)
-				return undefined // Return undefined in case of an error
-			}
-		},
+		fetchMore,
 	}
 }
