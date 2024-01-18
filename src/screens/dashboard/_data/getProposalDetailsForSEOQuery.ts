46e711c8b109f2a3580c44312340b35357ac6103
kryptocodes <srivatsantb@gmail.com>
2024-01-18 16:36:23 +0530

    fix: SEO query
    Signed-off-by: kryptocodes <srivatsantb@gmail.com>

--- src/screens/dashboard/_data/getProposalDetailsForSEOQuery.ts
+++ src/screens/dashboard/_data/getProposalDetailsForSEOQuery.ts
@@ -3,7 +3,7 @@ export const getProposalDetailsForSEOQuery = gql`# This query is used in getServ
 query getProposalDetailsForSEO($proposalId: String!) {
   grantApplication(_id: $proposalId) {
     id:_id
-    title: fieldFilterByRegex(filter: { field: "projectName" }) {
+    title: fieldFilterBySection(filter: { field: "projectName" }) {
       values {
         value
       }
