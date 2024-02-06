5b70571dc565da33223aa81127400a68e021c228
kryptocodes <srivatsantb@gmail.com>
2024-02-06 12:41:44 +0530

    chore: clean up
    Signed-off-by: kryptocodes <srivatsantb@gmail.com>

--- src/screens/dashboard/FundBuilderDrawer.tsx
+++ src/screens/dashboard/FundBuilderDrawer.tsx
@@ -226,11 +226,8 @@ function FundBuilderDrawer() {
 			return
 		}
 
-		// setAmounts(selectedProposalsData.map((p) => p?.milestones?.[0]?.amount ? parseInt(p?.milestones?.[0]?.amount) : 0))
 		setTos(selectedProposalsData.map((p) => getFieldString(p, 'applicantAddress') ?? ''))
 		setMilestoneIndices(selectedProposalsData.map((p) => p.milestones.findIndex((m) => parseFloat(m?.amountPaid) === 0) !== -1 ? p.milestones.findIndex((m) => parseFloat(m?.amountPaid) === 0) : 0))
-		// setAmounts(milestoneIndices.map((mi, i) => selectedProposalsData[i]?.milestones[mi]?.amount ? parseInt(selectedProposalsData[i]?.milestones[mi]?.amount) : 0))
-		//setAmounts(selectedProposalsData.map((p) => p.milestones.findIndex((m) => parseFloat(m?.amountPaid) === 0) !== -1 ? p.milestones.findIndex((m) => parseFloat(m?.amountPaid) === 0) : 0))
 		setAmounts(selectedProposalsData.map((p) => p.milestones.findIndex((m) => parseFloat(m?.amountPaid) === 0) !== -1 ? p.milestones.findIndex((m) => parseFloat(m?.amountPaid) === 0) : 0)?.map((mi, i) => selectedProposalsData[i]?.milestones[mi]?.amount ? parseInt(selectedProposalsData[i]?.milestones[mi]?.amount) : 0))
 	}, [selectedProposalsData])
 
