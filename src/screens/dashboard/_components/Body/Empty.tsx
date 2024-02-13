43a4a0a6dbdec24898b5f5ec5a6606a53d040f26
kryptocodes <srivatsantb@gmail.com>
2024-02-13 22:26:10 +0530

    feat: redirect users to new ton grants page
    Signed-off-by: kryptocodes <srivatsantb@gmail.com>

--- src/screens/dashboard/_components/Body/Empty.tsx
+++ src/screens/dashboard/_components/Body/Empty.tsx
@@ -6,10 +6,11 @@ import SetupNotificationModal from 'src/libraries/ui/SetupNotificationModal'
 import { copyGrantLink } from 'src/libraries/utils/copy'
 import { getSupportedChainIdFromSupportedNetwork, getSupportedChainIdFromWorkspace } from 'src/libraries/utils/validations'
 import { GrantsProgramContext } from 'src/pages/_app'
+import { disabledTonGrants, tonGrants } from 'src/screens/proposal_form/_utils/constants'
 
 function Empty() {
 	const buildComponent = () => {
-		return (
+		return disabledTonGrants.includes(grant?.id as string) ? (
 			<Flex
 				w='100%'
 				h='100%'
@@ -19,150 +20,183 @@ function Empty() {
 				align='center'
 				justify='center'>
 				<Text
+					fontWeight='500'
+					color='black.100'
 					variant='heading3'
-					fontWeight='500'>
-					{role === 'admin' ? 'Your invitation for proposals is live!' : 'Be the first to submit a proposal'}
+					textAlign='center'
+					mx={2}
+				>
+					All the grant information and proposals have moved to TON Grants page,
 
+					<Text
+						fontWeight='500'
+						variant='heading3'
+						as='a'
+						href={`${window.location.origin}/proposal_form/?grantId=${tonGrants}&chainId=10&newTab=true`}
+						target='_blank'
+						color='blue.500'
+						mx={2}>
+						please visit there for more info
+					</Text>
 				</Text>
 
-				{
-					role === 'admin' ? (
-						<>
-							<Text mt={1}>
-								Builders can start submitting proposals from today.
-							</Text>
 
-							<Text
-								mt={6}
-								fontWeight='500'>
-								Share with builders
-							</Text>
-							<Text
-								mt={1}
-								variant='body'>
-								Attract builders to
-								{' '}
-								{grant?.workspace?.title}
-								{' '}
-								with a link.
-							</Text>
-						</>
-					) : grant?.link ? (
-						<>
-							<Text variant='body'>
-								Read more about the grant
+			</Flex>
+		)
+			: (
+				<Flex
+					w='100%'
+					h='100%'
+					bg='white'
+					direction='column'
+					boxShadow='0px 2px 4px rgba(29, 25, 25, 0.1)'
+					align='center'
+					justify='center'>
+					<Text
+						variant='heading3'
+						fontWeight='500'>
+						{role === 'admin' ? 'Your invitation for proposals is live!' : 'Be the first to submit a proposal'}
+
+					</Text>
+
+					{
+						role === 'admin' ? (
+							<>
+								<Text mt={1}>
+									Builders can start submitting proposals from today.
+								</Text>
+
 								<Text
-									variant='body'
-									display='inline-block'
-									fontWeight={500}
-									marginLeft={1}
-									cursor='pointer'
-									onClick={
-										() => {
-											if(grant.link !== null) {
-												window.open(grant.link, '_blank')
+									mt={6}
+									fontWeight='500'>
+									Share with builders
+								</Text>
+								<Text
+									mt={1}
+									variant='body'>
+									Attract builders to
+									{' '}
+									{grant?.workspace?.title}
+									{' '}
+									with a link.
+								</Text>
+							</>
+						) : grant?.link ? (
+							<>
+								<Text variant='body'>
+									Read more about the grant
+									<Text
+										variant='body'
+										display='inline-block'
+										fontWeight={500}
+										marginLeft={1}
+										cursor='pointer'
+										onClick={
+											() => {
+												if(grant.link !== null) {
+													window.open(grant.link, '_blank')
+												}
 											}
 										}
-									}
-								>
-									here
+									>
+										here
+									</Text>
 								</Text>
-							</Text>
 
-						</>
+							</>
 
-					) : null
-				}
+						) : null
+					}
 
-				{
-					role === 'admin' ? (
-						<Button
-							variant='primaryMedium'
-							mt={6}
-							onClick={
-								async() => {
-									if(grant?.id) {
-										const ret = await copyGrantLink(grant.id, getSupportedChainIdFromWorkspace(grant.workspace) ?? defaultChainId)
-										toast({
-											title: ret ? 'Copied!' : 'Failed to copy',
-											status: ret ? 'success' : 'error',
-											duration: 3000,
-										})
-									}
-								}
-							}>
-							<Text
-								variant='body'
-								color='white'>
-								Copy Link
-							</Text>
-						</Button>
-					)
-						: (
+					{
+						role === 'admin' ? (
 							<Button
 								variant='primaryMedium'
 								mt={6}
 								onClick={
-									() => {
-										const href = window.location.href.split('/')
-										const protocol = href[0]
-										const domain = href[2]
-										const chainId = getSupportedChainIdFromSupportedNetwork(grant?.workspace.supportedNetworks[0])
-
-										const URL = `${protocol}//${domain}/proposal_form/?grantId=${grant?.id}&chainId=${chainId}&newTab=true`
-
-										window.open(URL, '_blank')
+									async() => {
+										if(grant?.id) {
+											const ret = await copyGrantLink(grant.id, getSupportedChainIdFromWorkspace(grant.workspace) ?? defaultChainId)
+											toast({
+												title: ret ? 'Copied!' : 'Failed to copy',
+												status: ret ? 'success' : 'error',
+												duration: 3000,
+											})
+										}
 									}
 								}>
 								<Text
 									variant='body'
-									fontWeight='500'
 									color='white'>
-									Submit proposal
+									Copy Link
 								</Text>
 							</Button>
 						)
-				}
+							: (
+								<Button
+									variant='primaryMedium'
+									mt={6}
+									onClick={
+										() => {
+											const href = window.location.href.split('/')
+											const protocol = href[0]
+											const domain = href[2]
+											const chainId = getSupportedChainIdFromSupportedNetwork(grant?.workspace.supportedNetworks[0])
 
-				{
-					role === 'admin' && (
-						<Flex
-							mt={10}
-							direction='column'
-							align='center'
-							px='10%'>
-							<Text fontWeight='500'>
-								Subscribe to notifications
-							</Text>
-							<Text
-								mt={1}
-								textAlign='center'
-								variant='body'>
-								Get notified on Telegram when builders submit new proposals , reviewers submit
-								reviews, and other updates from builders and community.
-							</Text>
-							<Button
-								variant='primaryMedium'
-								bg='gray.300'
-								mt={6}
-								onClick={() => setIsSetupNotificationModalOpen(true)}>
-								<Text
-									variant='body'
-									fontWeight='500'>
-									Subscribe
+											const URL = `${protocol}//${domain}/proposal_form/?grantId=${grant?.id}&chainId=${chainId}&newTab=true`
+
+											window.open(URL, '_blank')
+										}
+									}>
+									<Text
+										variant='body'
+										fontWeight='500'
+										color='white'>
+										Submit proposal
+									</Text>
+								</Button>
+							)
+					}
+
+					{
+						role === 'admin' && (
+							<Flex
+								mt={10}
+								direction='column'
+								align='center'
+								px='10%'>
+								<Text fontWeight='500'>
+									Subscribe to notifications
 								</Text>
-							</Button>
-						</Flex>
-					)
-				}
+								<Text
+									mt={1}
+									textAlign='center'
+									variant='body'>
+									Get notified on Telegram when builders submit new proposals , reviewers submit
+									reviews, and other updates from builders and community.
+								</Text>
+								<Button
+									variant='primaryMedium'
+									bg='gray.300'
+									mt={6}
+									onClick={() => setIsSetupNotificationModalOpen(true)}>
+									<Text
+										variant='body'
+										fontWeight='500'>
+										Subscribe
+									</Text>
+								</Button>
+							</Flex>
+						)
+					}
 
-				<SetupNotificationModal
-					isOpen={isSetupNotificationModalOpen}
-					onClose={() => setIsSetupNotificationModalOpen(false)}
-					type='grant'
-					grantId={grant?.id!} />
-			</Flex>
-		)
+					<SetupNotificationModal
+						isOpen={isSetupNotificationModalOpen}
+						onClose={() => setIsSetupNotificationModalOpen(false)}
+						type='grant'
+						grantId={grant?.id!} />
+				</Flex>
+			)
 	}
 
 	const { grant, role } = useContext(GrantsProgramContext)!
