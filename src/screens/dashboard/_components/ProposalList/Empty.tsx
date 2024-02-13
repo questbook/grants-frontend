24fcb91a467d94a259cea5bc9a448f85e5a99d59
kryptocodes <srivatsantb@gmail.com>
2024-02-14 00:05:10 +0530

    feat: redirect (ton grants) - mobile screen
    Signed-off-by: kryptocodes <srivatsantb@gmail.com>

--- src/screens/dashboard/_components/ProposalList/Empty.tsx
+++ src/screens/dashboard/_components/ProposalList/Empty.tsx
@@ -1,4 +1,7 @@
+import { useContext } from 'react'
 import { Flex, Text } from '@chakra-ui/react'
+import { GrantsProgramContext } from 'src/pages/_app'
+import { disabledTonGrants, tonGrants } from 'src/screens/proposal_form/_utils/constants'
 
 function Empty() {
 	const buildComponent = () => {
@@ -9,16 +12,43 @@ function Empty() {
 				px={5}
 				py={4}
 				align='center'>
-				<Text
-					my='auto'
-					mx='auto'
-					textAlign='center'>
-					Proposals from builders show up here.
-				</Text>
+				{
+					disabledTonGrants?.includes(grant?.id as string) && window?.innerWidth < 768 ? (
+						<Text
+							fontWeight='500'
+							color='black.100'
+							variant='heading3'
+							textAlign='center'
+							mx={2}
+						>
+							All the grant information and proposals have moved to TON Grants page,
+
+							<Text
+								fontWeight='500'
+								variant='heading3'
+								as='a'
+								href={`${window.location.origin}/proposal_form/?grantId=${tonGrants}&chainId=10&newTab=true`}
+								target='_blank'
+								color='blue.500'
+								mx={2}>
+								please visit there for more info
+							</Text>
+						</Text>
+					) : (
+						<Text
+							my='auto'
+							mx='auto'
+							textAlign='center'>
+							Proposals from builders show up here.
+						</Text>
+					)
+				}
+
 			</Flex>
 		)
 	}
 
+	const { grant } = useContext(GrantsProgramContext)!
 	return buildComponent()
 }
 
