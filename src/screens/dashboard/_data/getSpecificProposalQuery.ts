1df923fca452a0d5325aea1582287cffba6389dc
kryptocodes <srivatsantb@gmail.com>
2024-03-20 19:45:03 +0530

    feat: added Synaps id query
    Signed-off-by: kryptocodes <srivatsantb@gmail.com>

--- src/screens/dashboard/_data/getSpecificProposalQuery.ts
+++ src/screens/dashboard/_data/getSpecificProposalQuery.ts
@@ -7,6 +7,7 @@ export const getSpecificProposalQuery = gql`query getProposals($grantID: String!
       applicantId
       applicantPublicKey
       state
+      synapsId
       fields {
         id:_id
         values {
