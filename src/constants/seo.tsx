904ebe73ae4d57be35276fbd89eb0eaef65d288c
kryptocodes <srivatsantb@gmail.com>
2024-04-10 16:53:35 +0530

    chore: updated opengraph img
    Signed-off-by: kryptocodes <srivatsantb@gmail.com>

--- src/constants/seo.tsx
+++ src/constants/seo.tsx
@@ -4,18 +4,18 @@ const seoConfig = {
 	description:
       'Discover Opportunities in Web 3.0 and Earn in Crypto',
 	// siteUrl: 'https://www.questbook.app/',
-	siteUrl: 'https://beta.questbook.app/',
+	siteUrl: 'https://starknet.questbook.app/',
 	twitter: {
 		handle: '@questbookapp',
 		site: '@questbookapp',
 		cardType: 'summary_large_image',
 		title: 'Questbook Grant',
-		image: 'https://ipfs.io/ipfs/bafkreif2xxm6kfj3n5gksduce26xjyijhkh6qil7e3pj7yuysjaneh624y',
+		image: 'https://ipfs.io/ipfs/QmdKbYrRBC8cd2YdjEcPJFjWXtM9LDqu7eXoD4yqJ5cSQ3',
 	},
 	openGraph: {
 		type: 'website',
 		locale: 'en_US',
-		url: 'https://beta.questbook.app/',
+		url: 'https://starknet.questbook.app/',
 		title: 'Questbook',
 		description:
         'Discover Opportunities in Web 3.0 and Earn in Crypto',
@@ -23,13 +23,13 @@ const seoConfig = {
 		site_name: 'Questbook',
 		images: [
 			{
-				url: 'https://ipfs.io/ipfs/bafkreif2xxm6kfj3n5gksduce26xjyijhkh6qil7e3pj7yuysjaneh624y',
+				url: 'https://ipfs.io/ipfs/QmdKbYrRBC8cd2YdjEcPJFjWXtM9LDqu7eXoD4yqJ5cSQ3',
 				width: 1240,
 				height: 480,
 				alt: 'Questbook Grant',
 			},
 			{
-				url: 'https://ipfs.io/ipfs/bafkreif2xxm6kfj3n5gksduce26xjyijhkh6qil7e3pj7yuysjaneh624y',
+				url: 'https://ipfs.io/ipfs/QmdKbYrRBC8cd2YdjEcPJFjWXtM9LDqu7eXoD4yqJ5cSQ3',
 				width: 1012,
 				height: 506,
 				alt: 'Questbook Grant',
