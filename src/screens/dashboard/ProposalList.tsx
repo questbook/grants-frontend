c24f563d7af47c90efe55583ccfb16b94b1d4cce
kryptocodes <srivatsantb@gmail.com>
2024-05-02 21:28:57 +0530

    chore: remove loader
    Signed-off-by: kryptocodes <srivatsantb@gmail.com>

--- src/screens/dashboard/ProposalList.tsx
+++ src/screens/dashboard/ProposalList.tsx
@@ -196,17 +196,6 @@ function ProposalList({ step, setStep }: { step?: boolean, setStep?: (value: boo
 					})
 				}
 				{proposalCount === 0 && <Empty />}
-				{
-					(proposalCount !== proposals.length - 1) && (
-						<Button
-							my={4}
-							isLoading={true}
-							loadingText='Loading Proposals, please wait.'
-							variant='link'
-							cursor='default'
-						/>
-					)
-				}
 			</Flex>
 		</Flex>
 	)
