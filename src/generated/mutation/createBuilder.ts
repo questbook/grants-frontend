17af8f5752f944f40b87360d759884e6170bd389
kryptocodes <srivatsantb@gmail.com>
2024-05-22 21:50:57 +0530

    feat: add referral field
    Signed-off-by: kryptocodes <srivatsantb@gmail.com>

--- src/generated/mutation/createBuilder.ts
+++ src/generated/mutation/createBuilder.ts
@@ -1,6 +1,6 @@
 import { gql } from '@apollo/client'
-export const createBuilder = gql`mutation createBuilder($telegram: String!, $github: JSON!, $application: String, $email: String!, $twitter: String) {
-    createBuilder(telegram: $telegram, github: $github, application: $application, email: $email, twitter: $twitter) {
+export const createBuilder = gql`mutation createBuilder($telegram: String!, $github: JSON!, $application: String, $email: String!, $twitter: String, $referral: JSON) {
+    createBuilder(telegram: $telegram, github: $github, application: $application, email: $email, twitter: $twitter, referral: $referral) {
         recordId
         record {
             _id
