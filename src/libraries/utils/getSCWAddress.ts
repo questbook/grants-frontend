7a189589cbae5a580e118badaaa76a19bc4f0efc
kryptocodes <srivatsantb@gmail.com>
2024-10-10 10:25:24 +0530

    fix: alchemy api
    Signed-off-by: kryptocodes <srivatsantb@gmail.com>

--- src/libraries/utils/getSCWAddress.ts
+++ src/libraries/utils/getSCWAddress.ts
@@ -5,7 +5,7 @@ export const getSCWAddress = async(address: string) => {
 	try {
 		const scw = '0x050bca32264195976Fe00BcA566B548413A9E658'
 		const fetchAddress = async(provider: string, address: string) => {
-			const providerConfig = new ethers.providers.AlchemyProvider(provider, '8iYVobmm24k85ejRIS0fd8sBzSVbQtZv')
+			const providerConfig = new ethers.providers.AlchemyProvider(provider, 'YKJ826SHB7c7rV5dDzcubjX8LzYRVdvn')
 			const scwContract = new ethers.Contract(scw, ABI, providerConfig)
 			const result = await scwContract.getAddressForCounterfactualWallet(address, 0)
 			const check = await scwContract.isWalletExist(result)
