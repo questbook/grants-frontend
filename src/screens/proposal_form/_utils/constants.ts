1cda39da247d6ba3c77c381778f74cfce3ea67e3
kryptocodes <srivatsantb@gmail.com>
2024-10-11 00:09:34 +0530

    chore: add banner text
    Signed-off-by: kryptocodes <srivatsantb@gmail.com>

--- src/screens/proposal_form/_utils/constants.ts
+++ src/screens/proposal_form/_utils/constants.ts
@@ -5,6 +5,6 @@ export const DEFAULT_FORM: Form = { fields: [], milestones: [], members: [], det
 export const DEFAULT_MILESTONE = { index: 0, title: '', amount: 0 }
 export const MILESTONE_INPUT_STYLE = [{ placeholder: 'Add milestone', maxLength: 1024 }, { placeholder: 'Funding ask for this milestone' }]
 
-export const subdomains = ['661e3ca0f056dd981db4e4a5']
+export const subdomains = ['661e3ca0f056dd981db4e4a5', '67081a9b8868f5130a7c3896']
 export const customStepsHeader = ['Creating your proposal']
 export const customSteps = [ 'Validating form data', 'Uploading proposal data', 'Submitting your proposal']
