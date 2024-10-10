1e35a0bab6e99a57450f226eb04d5a55d5ee4886
kryptocodes <srivatsantb@gmail.com>
2024-10-10 10:26:10 +0530

    fix: alchemy api
    Signed-off-by: kryptocodes <srivatsantb@gmail.com>

--- src/libraries/utils/gasless.ts
+++ src/libraries/utils/gasless.ts
@@ -26,9 +26,9 @@ const EIP712_WALLET_TX_TYPE = {
 
 export const jsonRpcProviders: { [key: string]: ethers.providers.JsonRpcProvider } =
 {
-	'5': new ethers.providers.JsonRpcProvider('https://eth-goerli.g.alchemy.com/v2/c7FL3Wd0zxt_DtjeN1wqMWtCFVUTV_sP'),
-	'137': new ethers.providers.JsonRpcProvider('https://polygon-mainnet.g.alchemy.com/v2/zcWxjKH0Sif5PLp-b-vxP_M1JYF1aoPG'),
-	'10': new ethers.providers.JsonRpcProvider('https://opt-mainnet.g.alchemy.com/v2/8iYVobmm24k85ejRIS0fd8sBzSVbQtZv'),
+	'5': new ethers.providers.JsonRpcProvider('https://polygon-mainnet.g.alchemy.com/v2/YKJ826SHB7c7rV5dDzcubjX8LzYRVdvn'),
+	'137': new ethers.providers.JsonRpcProvider('https://polygon-mainnet.g.alchemy.com/v2/YKJ826SHB7c7rV5dDzcubjX8LzYRVdvn'),
+	'10': new ethers.providers.JsonRpcProvider('https://opt-mainnet.g.alchemy.com/v2/YKJ826SHB7c7rV5dDzcubjX8LzYRVdvn'),
 	'42220': new ethers.providers.JsonRpcProvider('https://forno.celo.org/'),
 }
 
