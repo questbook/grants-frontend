86bd2df203f757d013e7a27e1d832aaa2168b335
kryptocodes <srivatsantb@gmail.com>
2024-10-21 20:58:27 +0530

    fix: safe wallet version (support native tx - pol update)
    Signed-off-by: kryptocodes <srivatsantb@gmail.com>

--- src/libraries/logger.ts
+++ src/libraries/logger.ts
@@ -2,7 +2,7 @@ import P from 'pino'
 
 const logger = P()
 
-const defaultLogLevel = process.env.NODE_ENV === 'production' ? 'info' : 'debug'
+const defaultLogLevel = process.env.NODE_ENV === 'production' ? 'silent' : 'debug'
 const logLevel = process.env.LOG_LEVEL || defaultLogLevel
 logger.level = logLevel
 
